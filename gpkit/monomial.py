f945091a376bd6ff2b6bf2fd45e4993c23f7786c
Warren Hoburg <warren.w.hoburg@boeing.com>
2014-06-26 15:05:17 -0700

    pep8

--- gpkit/monomial.py
+++ gpkit/monomial.py
@@ -6,6 +6,7 @@ Monomial expression (term)
 #   since the monomial class only calls the Posynomial class
 #   when there's addition or subtraction, it might be alright?
 
+
 def monify(s):
     """
     Turns a whitespace separated string into singlet monomials.
@@ -16,7 +17,8 @@ def monify(s):
 class Monomial(object):
 
     def __init__(self, _vars, c=1, a=None):
-        if isinstance(_vars, str):  _vars = [_vars]
+        if isinstance(_vars, str):
+            _vars = [_vars]
         self.c = float(c)
         self.vars = set(_vars)
 
@@ -27,7 +29,8 @@ class Monomial(object):
             # ensure _vars is a list of variable names
             N = len(_vars)
             # if we don't have an exponent list, use the default
-            if a is None:  a = [1]*N
+            if a is None:
+                a = [1] * N
             # if we do have one, check that it's the right length
             assert N == len(a), 'N=%s but len(a)=%s' % (N, len(a))
             # zip 'em up!
@@ -47,9 +50,9 @@ class Monomial(object):
 
     def __repr__(self):
         return self._str_tokens()
-    
+
     def latex(self, bracket='$'):
-        latexstr = self._str_tokens('') # could put a space in here?
+        latexstr = self._str_tokens('')  # could put a space in here?
         return bracket + latexstr + bracket
 
     def __hash__(self):
@@ -58,12 +61,12 @@ class Monomial(object):
     def __eq__(self, m):
         """Equality test
 
-        Args: 
+        Args:
             m (Monomial): Monomial to compare with
 
         Returns:
             bool -- True if self == m
-        
+
         Notes:
             Currently returns True only if variables have same order
         """
@@ -74,7 +77,7 @@ class Monomial(object):
 
     def __pow__(self, x):
         """Put monomial to a numeric power
-        
+
         Args:
             x (float or int): exponent to put monomial to
 
@@ -85,13 +88,13 @@ class Monomial(object):
         a = []
         for var in self.vars:
             exp = self.exps[var]
-            a.append(exp*x)
-        c = self.c**x
+            a.append(exp * x)
+        c = self.c ** x
         return Monomial(self.vars, c, a)
 
     def __div__(self, m):
         """Division by another monomial
-        
+
         Args:
             m (Monomial): monomial to divide by
 
@@ -102,35 +105,34 @@ class Monomial(object):
             # assume m is numeric scalar
             m = Monomial([], c=m)
         _vars = self.vars.union(m.vars)
-        c = self.c/m.c
+        c = self.c / m.c
         a = [self.exps.get(var, 0) - m.exps.get(var, 0)
              for var in _vars]
         return Monomial(_vars, c, a)
 
     def __rdiv__(self, m):
         # m/self
-        return m * self**-1
+        return m * self ** -1
 
     def __mul__(self, m):
         """Multiplication by another monomial
-        
+
         Args:
             m (Monomial): monomial to multiply by
 
         Returns:
             Monomial
         """
-        return self/(m**-1)
+        return self / (m ** -1)
 
     def __rmul__(self, m):
-        return self*m
-
+        return self * m
 
     def __sub__(self, m):
-        return Posynomial([self, -1*m])
+        return Posynomial([self, -1 * m])
 
     def __rsub__(self, m):
-        return Posynomial([m, -1*self])
+        return Posynomial([m, -1 * self])
 
     def __add__(self, m):
         return Posynomial([self, m])
@@ -144,6 +146,6 @@ class Monomial(object):
 
     def __le__(self, m):
         # overloaded for returning constraints...
-        return self/m
+        return self / m
 
 from posynomial import Posynomial
--- gpkit/posynomial.py
+++ gpkit/posynomial.py
@@ -8,6 +8,7 @@ Posynomial expression (term)
 #   since the monomial class only calls the Posynomial class
 #   when there's addition or subtraction, it might be alright?
 
+
 class Posynomial(object):
     def __init__(self, posynomials):
 
@@ -27,17 +28,17 @@ class Posynomial(object):
         self.monomials = set(monomials)
 
         if len(self.monomials) <= 1:
-            raise TypeError, "Need more than one monomial to make a posynomial"
+            raise TypeError("Need more than one monomial to make a posynomial")
 
-        vars = []
+        _vars = []
         for m in self.monomials:
-            vars += m.vars
-        self.vars = set(vars)
-    
+            _vars += m.vars
+        self.vars = set(_vars)
+
     def __repr__(self):
         strlist = [str(m) for m in self.monomials]
         return ' + '.join(strlist)
-    
+
     def latex(self, bracket='$'):
         latexstr = ' + '.join([m.latex('') for m in self.monomials])
         return bracket + latexstr + bracket
@@ -45,34 +46,32 @@ class Posynomial(object):
     def __hash__(self):
         return hash(str(self))
 
-
     def __eq__(self, p):
         return (isinstance(p, Posynomial) and str(self) == str(p))
 
     def __ne__(self, x):
         return not self.__eq__(x)
 
-
     def __pow__(self, x):
-        return Posynomial([m**x for m in self.monomials])
+        return Posynomial([m ** x for m in self.monomials])
 
     def __div__(self, m):
-        return Posynomial([m_s/m for m_s in self.monomials])
+        return Posynomial([m_s / m for m_s in self.monomials])
 
     def __rdiv__(self, m):
-        return Posynomial([m/m_s for m_s in self.monomials])
+        return Posynomial([m / m_s for m_s in self.monomials])
 
     def __mul__(self, m):
-        return Posynomial([m_s*m for m_s in self.monomials])
+        return Posynomial([m_s * m for m_s in self.monomials])
 
     def __rmul__(self, m):
-        return Posynomial([m*m_s for m_s in self.monomials])
+        return Posynomial([m * m_s for m_s in self.monomials])
 
     def __sub__(self, m):
-        return Posynomial([self, -1*m])
+        return Posynomial([self, -1 * m])
 
     def __rsub__(self, m):
-        return Posynomial([m, -1*self])
+        return Posynomial([m, -1 * self])
 
     def __add__(self, m):
         return Posynomial([self, m])
@@ -85,6 +84,6 @@ class Posynomial(object):
 
     def __le__(self, m):
         # overloaded for returning constraints...
-        return self/m
+        return self / m
 
 from monomial import Monomial
