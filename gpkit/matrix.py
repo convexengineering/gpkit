8cf40d5275abf723d6911917150b44de6740d501
Edward Burnell <eburn@mit.edu>
2014-06-26 22:46:32 -0700

    swapping out lambdas

--- gpkit/matrix.py
+++ gpkit/matrix.py
@@ -2,19 +2,18 @@ import numpy as np
 
 class matrix(np.matrix):
     # change printing
-    __repr__ = lambda self: str(self)
-    __eq__ = lambda self, m: (isinstance(m, self.__class__)
-                              and str(self) == str(m))
-    __ne__ = lambda self, m: not self == m
+    def __repr__(self): return str(self)
+    def __eq__(self, m): (isinstance(m, self.__class__)
+                                and str(self) == str(m))
+    def __ne__(self, m): not self == m
 
     # element-wise operators
-    pow = lambda self, x: np.power(self, x)
-    div = lambda self, x: np.divide(self, x)
-    mul = lambda self, x: np.multiply(self, x)
+    def pow(self, x): return np.power(self, x)
+    def div(self, x): return np.divide(self, x)
+    def mul(self, x): return np.multiply(self, x)
 
     # constraint generators
     _leq = np.vectorize(lambda a, b: a <= b)
-    __lt__ = lambda self, x: self <= x
-    __le__ = lambda self, x: [e if len(e)>1
-                                else e[0]
-                              for e in self._leq(self, x).tolist()]
+    def __lt__(self, x): return self <= x
+    def __le__(self, x): return [e if len(e) > 1 else e[0]
+                                 for e in self._leq(self, x).tolist()]
