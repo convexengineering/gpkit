fd82e0c337103be02ce094fae03ba3f0c445db6e
Edward Burnell <eburn@mit.edu>
2014-06-26 21:27:24 -0700

    removed matrix bits

--- gpkit/__init__.py
+++ gpkit/__init__.py
@@ -3,11 +3,9 @@ Lightweight GP Modeling Package
 """
 
 # structures
-from matrix import matrix
 from nomials import Monomial
 from nomials import Posynomial
 
 # functions
 from utils import monify
-from utils import vectify	
 from minimize import chooser as minimize
--- gpkit/utils.py
+++ gpkit/utils.py
@@ -1,12 +1,7 @@
 from nomials import Monomial
-from matrix import matrix
 
 def monify(s):
     """
     Turns a whitespace separated string into singlet monomials.
     """
     return [Monomial(x) for x in s.split()]
-
-def vectify(s,n):
-    assert len(s.split()) == 1, "Accepts only a single variable name."
-    return matrix([Monomial(s+str(i)) for i in xrange(n)]).T
