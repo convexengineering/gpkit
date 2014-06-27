23ea0649f026ee824c42d81c17fa353c5eb42220
Edward Burnell <eburn@mit.edu>
2014-06-26 21:04:12 -0700

    Added many tests for upcoming features:
      - vector and constraint tests
    
      - changed tests to match nomials dictionary interface

--- gpkit/tests/test_gpkit.py
+++ gpkit/tests/test_gpkit.py
@@ -1,7 +1,6 @@
 import unittest
-from gpkit.monomial import Monomial
-from gpkit.monomial import monify
-from gpkit.posynomial import Posynomial
+from gpkit import *
+from collections import defaultdict
 
 
 class Test_Monomial(unittest.TestCase):
@@ -10,10 +9,10 @@ class Test_Monomial(unittest.TestCase):
         pass
 
     def test_init(self):
-        m = Monomial(['x', 'y'], 5, [1, -1])
-        m2 = Monomial({'x': 1, 'y': -1}, 5)
+        m = Monomial({'x':2, 'y':-1}, 5)
+        m2 = Monomial({'x': 2, 'y': -1}, 5)
         self.assertEqual(m.vars, set(['x', 'y']))
-        self.assertEqual(m.exps, {'x': 1, 'y': -1})
+        self.assertEqual(m.exps, {'x': 2, 'y': -1})
         self.assertEqual(m.c, 5)
         self.assertEqual(m, m2)
 
@@ -24,12 +23,12 @@ class Test_Monomial(unittest.TestCase):
         self.assertEqual(m.c, 1)
 
     def test_repr(self):
-        m = Monomial(['x', 'y'], 5, [2, -1])
+        m = Monomial({'x':2, 'y':-1}, 5)
         self.assertEqual(type(m.__repr__()), str)
         self.assertEqual(Monomial('x').__repr__(), 'x')
 
     def test_latex(self):
-        m = Monomial(['x', 'y'], 5, [2, -1]).latex()
+        m = Monomial({'x':2, 'y':-1}, 5).latex()
         self.assertEqual(type(m), str)
         self.assertTrue('$' in m, '$ not in %s' % m)
         self.assertEqual(Monomial('x', 5).latex(), '$5x$')
@@ -49,56 +48,58 @@ class Test_Monomial(unittest.TestCase):
         self.assertFalse(x != x)
 
         # scalar fails on type comparison even though c matches
-        x = Monomial([], c=1)
+        x = Monomial({}, 1)
         y = 1
         self.assertFalse(x == y)
 
         # several vars
-        m1 = Monomial(['a', 'b', 'c'], c=5, a=[3, 2, 1])
-        m2 = Monomial(['a', 'b', 'c'], c=5., a=[3, 2, 1])
-        m3 = Monomial(['a', 'b', 'c'], c=6, a=[3, 2, 1])
-        m4 = Monomial(['a', 'b'], c=5, a=[3, 2])
+        m1 = Monomial({'a':3, 'b':2, 'c':1}, 5)
+        m2 = Monomial({'a':3, 'b':2, 'c':1}, 5)
+        m3 = Monomial({'a':3, 'b':2, 'c':1}, 6)
+        m4 = Monomial({'a':3, 'b':2}, 5)
         self.assertTrue(m1 == m2)
         self.assertTrue(m1 != m3)
         self.assertTrue(m1 != m4)
 
     def test_div(self):
-        x = Monomial(['x', 'y'], 36, [1, -1])
+        x, y, z, t = monify('x y z t')
+        a = 36*x/y
         # divide by scalar
-        self.assertEqual(x/9, Monomial(['x', 'y'], 4, [1, -1]))
+        self.assertEqual(a/9, 4*x/y)
         # divide by Monomial
-        y = x / Monomial('z')
-        self.assertEqual(y, Monomial(['x', 'y', 'z'], 36, [1, -1, -1]))
+        b = a / z
+        self.assertEqual(b, 36*x/y/z)
         # make sure x unchanged
-        self.assertEqual(x, Monomial(['x', 'y'], 36, [1, -1]))
+        self.assertEqual(a, Monomial({'x':1, 'y':-1}, 36))
         # mixed new and old vars
-        z = x / Monomial(['x', 't'], .5, [-1, 2])
-        self.assertEqual(z, Monomial(['x', 'y', 't'], 72, [2, -1, -2]))
+        c = a / (0.5*t**2/x)
+        self.assertEqual(c, Monomial({'x':2, 'y':-1, 't':-2}, 72))
 
     def test_mul(self):
-        x = Monomial(['x', 'y'], 4, [1, -1])
+        x = Monomial({'x':1, 'y':-1}, 4)
         # divide by scalar
-        self.assertEqual(x*9, Monomial(['x', 'y'], 36, [1, -1]))
+        self.assertEqual(x*9, Monomial({'x':1, 'y':-1}, 36))
         # divide by Monomial
         y = x * Monomial('z')
-        self.assertEqual(y, Monomial(['x', 'y', 'z'], 4, [1, -1, 1]))
+        self.assertEqual(y, Monomial({'x':1, 'y':-1, 'z':1}, 4))
         # make sure x unchanged
-        self.assertEqual(x, Monomial(['x', 'y'], 4, [1, -1]))
+        self.assertEqual(x, Monomial({'x':1, 'y':-1}, 4))
         # mixed new and old vars
-        z = x * Monomial(['x', 't'], .5, [-1, 2])
-        self.assertEqual(z, Monomial(['x', 'y', 't'], 2, [0, -1, 2]))
+        z = x * Monomial({'x':-1, 't':2}, .5)
+        self.assertEqual(z, Monomial({'x':0, 'y':-1, 't':2}, 2))
 
     def test_pow(self):
-        x = Monomial(['x', 'y'], 4, [1, -1])
+        x = Monomial({'x':1, 'y':-1}, 4)
+        self.assertEqual(x, Monomial({'x':1, 'y':-1}, 4))
         # identity
         self.assertEqual(x/x, Monomial({}, 1))
         # square
         self.assertEqual(x*x, x**2)
         # divide
-        y = Monomial(['x', 'y'], 5, [2, 3])
+        y = Monomial({'x':2, 'y':3}, 5)
         self.assertEqual(x/y, x*y**-1)
         # make sure x unchanged
-        self.assertEqual(x, Monomial(['x', 'y'], 4, [1, -1]))
+        self.assertEqual(x, Monomial({'x':1, 'y':-1}, 4))
 
 
 class Test_Posynomial(unittest.TestCase):
@@ -116,18 +117,78 @@ class Test_Posynomial(unittest.TestCase):
         p2 = 3.14*x*y**2 + y/2 - x**3*6*y + 2
         self.assertEqual(p, p2)
 
+    def test_simplification(self):
+        x, y = monify('x y')
+        p1 = x+y+y+(x+y)+(y+x**2)-3*x
+        p2 = 4*y+x**2-x
+        self.assertEqual(p1, p2)
 
-class Test_monify(unittest.TestCase):
+    def test_posyposy_mult(self):
+        x, y = monify('x y')
+        p =  x**2 + 2*y*x + y**2
+        self.assertEqual((x+y)**2, p)
+        p2 =  2*x**2 + 2*y*x + y**2*x + y**3
+        self.assertEqual((x+y)*(2*x+y**2), p2)
+
+    def test_constraint_gen(self):
+        x, y = monify('x y')
+        p = x**2 + 2*y*x + y**2
+        self.assertEqual(p<=1, p)
+        self.assertEqual(p<x, p/x)
+
+
+class Test_utils(unittest.TestCase):
 
     def test_monify(self):
         x, y = monify('x y')
         self.assertEqual(x, Monomial('x'))
         self.assertEqual(y, Monomial('y'))
 
+    def test_vectify(self):
+        x = vectify('x', 3)
+        x2 = array(monify('x0 x1 x2'))
+        self.assertEqual(x, x2)
+
+
+class Test_matrices(unittest.TestCase):
+
+    def test_array_mult(self):
+        x = vectify('x', 3)
+        x0, x1, x2  = monify('x0 x1 x2')
+        p = x0**2 + x1**2 + x2**2
+        self.assertEqual(x.dot(x), p)
+        m = array([[x0**2, x0*x1, x0*x2],
+                    [x0*x1, x1**2, x1*x2],
+                    [x0*x2, x1*x2, x2**2]])
+        self.assertEqual(x.outer(x), m)
+
+    def test_elementwise_mult(self):
+        x = vectify('x', 3)
+        x0, x1, x2  = monify('x0 x1 x2')
+        # multiplication
+        v = array([1, 2, 3]).T
+        p = array([1*x0, 2*x1, 3*x2]).T
+        self.assertEqual(x*v, p)
+        # division
+        p2 = array([x0, x1/2, x2/3]).T
+        self.assertEqual(x/v, p2)
+        # power
+        p3 = array([x0**2, x1**2, x2**2]).T
+        self.assertEqual(x**2, p3)
+
+    def test_constraint_gen(self):
+        x = vectify('x', 3)
+        x0, x1, x2  = monify('x0 x1 x2')
+        v = array([1, 2, 3]).T
+        p = [x0, x1/2, x2/3]
+        self.assertEqual(x<=v, p)
+        self.assertEqual(x<v, p)
+
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
     loader = unittest.TestLoader()
-    for t in [Test_Monomial, Test_monify, Test_Posynomial]:
+    for t in [Test_utils, Test_Monomial,
+              Test_Posynomial, Test_matrices]:
         suite.addTests(loader.loadTestsFromTestCase(t))
     unittest.TextTestRunner(verbosity=2).run(suite)
