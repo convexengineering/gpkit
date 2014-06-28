741c6540d095af899d6c81267c01a95e7756dbcb
Edward Burnell <eburn@mit.edu>
2014-06-27 18:16:17 -0700

    added Mosek CLI interface

--- gpkit/minimize.py
+++ gpkit/minimize.py
@@ -1,31 +1,95 @@
+import numpy as np
+from scipy.sparse import dok_matrix
+from scipy import float32
+from itertools import chain
+
 def chooser(cost, constraints, solver, options):
+    posynomials = [cost]+constraints
+
+    freevars = frozenset().union(*[p.vars for p in posynomials])
+    monomials = list(chain(*[p.monomials for p in posynomials]))
+
+    # c: constant coefficients of the various monomials in F
+    c = np.array([m.c for m in monomials])
+    assert all(c>0), "Negative coefficients are not allowed:\n%s"%c
+
+    # k: number of monomials (columns of F) present in each constraint
+    k = [len(p.monomials) for p in posynomials]
+
+    # A: exponents of the various free variables for each monomial
+    #    rows of A are variables, columns are monomials
+    A = dok_matrix((len(freevars),len(monomials)), dtype=float32)
+    for i, v in enumerate(freevars):
+        for j, m in enumerate(monomials):
+            if m.exps[v]: A[i,j] = m.exps[v]
+    A = A.tocoo()
+
+    # map: posynomial index (0 for cost function)
+    i  = 0
+    map_ = []
+    for l in k:
+        map_ += [i]*l
+        i += 1
+
     if solver == 'cvxopt':
-        return cvxopt_solver([cost] + constraints, options)
+        solution = cvxopt_solver(c, A, k, options)
+    elif solver == "mosek_cli":
+        filename = options.get('filename', 'gpkit_mosek')
+        solution = mosek_cli(c, A, map_, filename)
     else:
         raise Exception("That solver is not implemented!")
 
+    return dict(zip(freevars, solution))
 
-def cvxopt_solver(posynomials, options):
-    from cvxopt import matrix, log, exp, solvers
-    from itertools import chain
+
+def cvxopt_solver(c, A, k, options):
+    from cvxopt import sparse, matrix, log, exp
 
     solvers.options.update(options)
 
-    freevars = set().union(*[p.vars for p in posynomials])
-    monomials = list(chain(*[p.monomials for p in posynomials]))
+    k = k
+    g = log(matrix(c))
+    F = sparse(A.data, A.row, A.column)
 
-    # # CVXopt stuff ##
-    # K: number of monomials (columns of F) present in each constraint
-    K = [len(p.monomials) for p in posynomials]
-    # g: constant coefficients of the various monomials in F
-    g = log(matrix([m.c for m in monomials]))
-    # F: exponents of the various free variables for each monomial
-    F = matrix([[float(m.exps.get(v, 0))
-                 for m in monomials]
-                for v in freevars])
-    # For more details on the format of these matrixes:
-    #  http://cvxopt.org/userguide/solvers.html?highlight=gp#cvxopt.solvers.gp
-
-    solution = solvers.gp(K, F, g)
+    solution = solvers.gp(k, F, g)
     # TODO: catch errors, delays, etc.
-    return dict(zip(freevars, exp(solution['x'])))
+    return exp(solution['x'])
+
+
+from subprocess import check_output
+
+def mosek_cli(c, A, map_, filename):
+    with open(filename, 'w') as f:
+        numcon = 1+map_[-1]
+        numter, numvar = map(int, A.shape)
+        for n in [numcon, numter, numvar]:
+            f.write("%d\n" % n)
+
+        f.write("\n*c\n")
+        f.writelines(["%.20e\n" % x for x in c])
+
+        f.write("\n*map_\n")
+        f.writelines(["%d\n" % x for x in map_])
+
+        t_j_Atj = np.array([A.col, A.row, A.data]).T.tolist()
+
+        f.write("\n*t  j  A_tj\n")
+        f.writelines(["%d %d %.20e\n" % tuple(x)
+                    for x in t_j_Atj])
+
+    check_output("mskexpopt "+filename, shell=True)
+
+    with open(filename+".sol") as f:
+        assert f.readline() == "PROBLEM STATUS      : PRIMAL_AND_DUAL_FEASIBLE\n"
+        assert f.readline() == "SOLUTION STATUS     : OPTIMAL\n"
+        f.readline()
+        f.readline()
+        f.readline()
+        f.readline()
+        vals = []
+        for line in f:
+            if line == "\n": break
+            else:
+                idx, val = line.split()
+                vals.append(np.exp(float(val)))
+        return vals
