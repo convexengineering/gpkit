2d2abfc34c7a921fca3cfb1d5d445401f739dcd7
Edward Burnell <eburn@mit.edu>
2014-06-30 00:22:29 -0700

    Changed the testing folder's organization

--- gpkit/tests/t_nomials.py
+++ gpkit/tests/t_nomials.py
@@ -1,11 +1,10 @@
 import math
 import unittest
 from gpkit import Monomial, Posynomial
-from gpkit.array import array
-from gpkit.utils import monify, vectify
+from gpkit.utils import monify
 
 
-class Test_Monomial(unittest.TestCase):
+class t_Monomial(unittest.TestCase):
 
     def setUp(self):
         pass
@@ -127,7 +126,7 @@ class Test_Monomial(unittest.TestCase):
                          4*math.log(c1) + 3*math.log(c2))
 
 
-class Test_Posynomial(unittest.TestCase):
+class t_Posynomial(unittest.TestCase):
 
     def test_basic(self):
         x, y = monify('x y')
@@ -162,58 +161,13 @@ class Test_Posynomial(unittest.TestCase):
         self.assertEqual(p < x, p/x)
 
 
-class Test_utils(unittest.TestCase):
-
-    def test_monify(self):
-        x, y = monify('x y')
-        self.assertEqual(x, Monomial('x'))
-        self.assertEqual(y, Monomial('y'))
-
-    def test_vectify(self):
-        x = vectify('x', 3)
-        x2 = array(monify('x0 x1 x2'))
-        self.assertEqual(x, x2)
-
-
-class Test_matrices(unittest.TestCase):
-
-    def test_array_mult(self):
-        x = vectify('x', 3)
-        x0, x1, x2 = monify('x0 x1 x2')
-        p = x0**2 + x1**2 + x2**2
-        self.assertEqual(x.dot(x), p)
-        m = array([[x0**2, x0*x1, x0*x2],
-                   [x0*x1, x1**2, x1*x2],
-                   [x0*x2, x1*x2, x2**2]])
-        self.assertEqual(x.outer(x), m)
-
-    def test_elementwise_mult(self):
-        x = vectify('x', 3)
-        x0, x1, x2 = monify('x0 x1 x2')
-        # multiplication
-        v = array([1, 2, 3]).T
-        p = array([1*x0, 2*x1, 3*x2]).T
-        self.assertEqual(x*v, p)
-        # division
-        p2 = array([x0, x1/2, x2/3]).T
-        self.assertEqual(x/v, p2)
-        # power
-        p3 = array([x0**2, x1**2, x2**2]).T
-        self.assertEqual(x**2, p3)
-
-    def test_constraint_gen(self):
-        x = vectify('x', 3)
-        x0, x1, x2 = monify('x0 x1 x2')
-        v = array([1, 2, 3]).T
-        p = [x0, x1/2, x2/3]
-        self.assertEqual(x <= v, p)
-        self.assertEqual(x < v, p)
-
+tests = [t_Posynomial, t_Monomial]
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
     loader = unittest.TestLoader()
-    for t in [Test_utils, Test_Monomial,
-              Test_Posynomial, Test_matrices]:
+
+    for t in tests:
         suite.addTests(loader.loadTestsFromTestCase(t))
+
     unittest.TextTestRunner(verbosity=2).run(suite)
---
+++ gpkit/tests/t_utils.py
@@ -0,0 +1,32 @@
+import unittest
+from gpkit import Monomial, Posynomial
+from gpkit.utils import monify, vectify
+from gpkit.array import array
+
+
+class t_monify(unittest.TestCase):
+
+    def test_monify(self):
+        x, y = monify('x y')
+        self.assertEqual(x, Monomial('x'))
+        self.assertEqual(y, Monomial('y'))
+
+
+class t_vectify(unittest.TestCase):
+
+    def test_vectify(self):
+        x = vectify('x', 3)
+        x2 = array(monify('x0 x1 x2'))
+        self.assertEqual(x, x2)
+
+
+tests = [t_monify, t_vectify]
+
+if __name__ == '__main__':
+    suite = unittest.TestSuite()
+    loader = unittest.TestLoader()
+
+    for t in tests:
+        suite.addTests(loader.loadTestsFromTestCase(t))
+
+    unittest.TextTestRunner(verbosity=2).run(suite)
