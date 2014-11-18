21299500a4ae5fff1ae54b84d0718364b2f72537
Edward Burnell <eburn@mit.edu>
2014-11-18 10:56:38 -0500

    Check for existence of pint in examples
    Closes #100, #101

--- gpkit/examples/simple_sweep.py
+++ gpkit/examples/simple_sweep.py
@@ -16,45 +16,36 @@ profile.enable()
 import numpy as np
 
 import gpkit
+mon = gpkit.mon
 
-pi = gpkit.Monomial("\\pi", units="", label="half of the circle constant")
-CDA0 = gpkit.Monomial("(CDA0)", units="cm^2", label="fuselage drag area")
-rho = gpkit.Monomial("\\rho", units="kg/m^3", label="density of air")
-mu = gpkit.Monomial("\\mu", units="kg/m/s", label="viscosity of air")
-S_wetratio = gpkit.Monomial("(\\frac{S}{S_{wet}})", units="", label="wetted area ratio")
-k = gpkit.Monomial("k", units="", label="form factor")
-e = gpkit.Monomial("e", units="", label="Oswald efficiency factor")
-W_0 = gpkit.Monomial("W_0", units="kN", label="aircraft weight excluding wing")
-N_ult = gpkit.Monomial("N_{ult}", units="", label="ultimate load factor")
-tau = gpkit.Monomial("\\tau", units="", label="airfoil thickness to chord ratio")
-C_Lmax = gpkit.Monomial("C_{L,max}", units="", label="max CL with flaps down")
-V_min = gpkit.Monomial("V_{min}", units="m/s", label="takeoff speed")
+pi = mon("\\pi", np.pi, "-", "half of the circle constant")
+rho = mon("\\rho", 1.23, "kg/m^3", "density of air")
+mu = mon("\\mu", 1.78e-5, "kg/m/s", "viscosity of air")
+S_wetratio = mon("(\\frac{S}{S_{wet}})", 2.05, "-", "wetted area ratio")
+k = mon("k", 1.2, "-", "form factor")
+e = mon("e", 0.95, "-", "Oswald efficiency factor")
+N_ult = mon("N_{ult}", 3.8, "-", "ultimate load factor")
+tau = mon("\\tau", 0.12, "-", "airfoil thickness to chord ratio")
+C_Lmax = mon("C_{L,max}", 1.5, "-", "max CL with flaps down")
 
-A = gpkit.Monomial("A", units="", label="aspect ratio")
-S = gpkit.Monomial("S", units="m^2", label="total wing area")
-C_D = gpkit.Monomial("C_D", units="", label="Drag coefficient of wing")
-C_L = gpkit.Monomial("C_L", units="", label="Lift coefficent of wing")
-C_f = gpkit.Monomial("C_f", units="", label="skin friction coefficient")
-Re = gpkit.Monomial("Re", units="", label="Reynold's number")
-W = gpkit.Monomial("W", units="N", label="total aircraft weight")
-W_w = gpkit.Monomial("W_w", units="N", label="wing weight")
-V = gpkit.Monomial("V", units="m/s", label="cruising speed")
+if gpkit.units:
+    CDA0 = mon("(CDA0)", 310.0, "cm^2", "fuselage drag area")
+    W_0 = mon("W_0", 4.94, "kN", "aircraft weight excluding wing")
+else:
+    CDA0 = mon("(CDA0)", 0.031, "m^2", "fuselage drag area")
+    W_0 = mon("W_0", 4940.0, "N", "aircraft weight excluding wing")
 
-substitutions = {
-    pi: np.pi,
-    CDA0: 310.0,
-    rho: 1.23,
-    mu: 1.78e-5,
-    S_wetratio: 2.05,
-    k: 1.2,
-    e: 0.95,
-    W_0: 4.94,
-    N_ult: 3.8,
-    tau: 0.12,
-    C_Lmax: 1.5,
-    V_min: ("sweep", np.linspace(20, 25, 3)),
-    V: ("sweep", np.linspace(45, 55, 3)),
-}
+V_min = mon("V_{min}", np.linspace(20, 25, 3), "m/s", "takeoff speed")
+V = mon("V", np.linspace(45, 55, 3), "m/s", "cruising speed")
+
+A = mon("A", "-", "aspect ratio")
+S = mon("S", "m^2", "total wing area")
+C_D = mon("C_D", "-", "Drag coefficient of wing")
+C_L = mon("C_L", "-", "Lift coefficent of wing")
+C_f = mon("C_f", "-", "skin friction coefficient")
+Re = mon("Re", "", "Reynold's number")
+W = mon("W", "N", "total aircraft weight")
+W_w = mon("W_w", "kN" if gpkit.units else "N", "wing weight")
 
 # drag modeling #
 C_D_fuse = CDA0/S             # fuselage viscous drag
@@ -62,8 +53,12 @@ C_D_wpar = k*C_f*S_wetratio  # wing parasitic drag
 C_D_ind = C_L**2/(pi*A*e)     # induced drag
 
 # wing-weight modeling #
-W_w_surf = 45.24*S*gpkit.units.parse_expression('N/m^2')  # surface weight
-W_w_strc = 8.71e-5*(N_ult*A**1.5*(W_0*W*S)**0.5)/tau / gpkit.units.m  # structural weight
+if gpkit.units:
+    W_w_strc = 8.71e-5*(N_ult*A**1.5*(W_0*W*S)**0.5)/tau / gpkit.units.m
+    W_w_surf = (45.24*gpkit.units.Pa) * S
+else:
+    W_w_strc = 8.71e-5*(N_ult*A**1.5*(W_0*W*S)**0.5)/tau
+    W_w_surf = 45.24 * S
 
 gp = gpkit.GP(  # minimize
                 0.5*rho*S*C_D*V**2,
@@ -74,8 +69,7 @@ gp = gpkit.GP(  # minimize
                     W <= 0.5*rho*S*C_Lmax*V_min**2,
                     W >= W_0 + W_w,
                     W_w >= W_w_surf + W_w_strc,
-                    C_D >= C_D_fuse + C_D_wpar + C_D_ind
-                ], substitutions)
+                    C_D >= C_D_fuse + C_D_wpar + C_D_ind, ])
 
 sol = gp.solve()
 
--- gpkit/tests/simpleflight_gps.py
+++ gpkit/tests/simpleflight_gps.py
@@ -5,17 +5,22 @@ import gpkit
 mon = gpkit.mon
 
 pi = mon("\\pi", np.pi, "-", "half of the circle constant")
-CDA0 = mon("(CDA0)", 310.0, "cm^2", "fuselage drag area")
 rho = mon("\\rho", 1.23, "kg/m^3", "density of air")
 mu = mon("\\mu", 1.78e-5, "kg/m/s", "viscosity of air")
 S_wetratio = mon("(\\frac{S}{S_{wet}})", 2.05, "-", "wetted area ratio")
 k = mon("k", 1.2, "-", "form factor")
 e = mon("e", 0.95, "-", "Oswald efficiency factor")
-W_0 = mon("W_0", 4.94, "kN", "aircraft weight excluding wing")
 N_ult = mon("N_{ult}", 3.8, "-", "ultimate load factor")
 tau = mon("\\tau", 0.12, "-", "airfoil thickness to chord ratio")
 C_Lmax = mon("C_{L,max}", 1.5, "-", "max CL with flaps down")
 V_min = mon("V_{min}", 22, "m/s", "takeoff speed")
+
+if gpkit.units:
+    CDA0 = mon("(CDA0)", 310.0, "cm^2", "fuselage drag area")
+    W_0 = mon("W_0", 4.94, "kN", "aircraft weight excluding wing")
+else:
+    CDA0 = mon("(CDA0)", 0.031, "m^2", "fuselage drag area")
+    W_0 = mon("W_0", 4940.0, "N", "aircraft weight excluding wing")
 
 D = mon("D", "N", "total drag force")
 A = mon("A", "-", "aspect ratio")
@@ -35,9 +40,12 @@ C_D_wpar = k*C_f*S_wetratio
 C_D_ind = C_L**2/(pi*A*e)
 equations += [C_D >= C_D_fuse + C_D_wpar + C_D_ind]
 
-
-W_w_strc = 8.71e-5*(N_ult*A**1.5*(W_0*W*S)**0.5)/tau / gpkit.units.m
-W_w_surf = (45.24*gpkit.units.Pa) * S
+if gpkit.units:
+    W_w_strc = 8.71e-5*(N_ult*A**1.5*(W_0*W*S)**0.5)/tau / gpkit.units.m
+    W_w_surf = (45.24*gpkit.units.Pa) * S
+else:
+    W_w_strc = 8.71e-5*(N_ult*A**1.5*(W_0*W*S)**0.5)/tau
+    W_w_surf = 45.24 * S
 equations += [W_w >= W_w_surf + W_w_strc]
 
 equations += [D >= 0.5*rho*S*C_D*V**2,
