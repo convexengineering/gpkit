22a25a746d46db672c31d3c4538354574b81d72e
Edward Burnell <eburn@mit.edu>
2014-11-22 23:26:37 -0500

    tiny fix to interactive widgets

--- gpkit/interactive/widget.py
+++ gpkit/interactive/widget.py
@@ -5,9 +5,14 @@ def widget(gp, outputfn, ranges):
     gp.prewidget = gp.last
 
     def display(**kwargs):
-        gp.sub(kwargs)
+        subs = {}
+        varkeys = gp.var_locs.keys()
+        for key, value in kwargs.items():
+            if key in varkeys:
+                subs[varkeys[varkeys.index(key)]] = value
+        gp.sub(subs)
         gp.solve(printing=False)
         outputfn(gp)
-        gp.load(gp.prewidget, print_boundwarnings=False)
+        gp.load(gp.prewidget, printing=False)
 
     return interactive(display, **ranges)
