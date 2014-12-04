f0f81a60cd0b72cb329a335d1a41184fe2ef2cdd
Edward Burnell <eburn@mit.edu>
2014-12-03 20:27:47 -0500

    Add no-units tests

--- gpkit/tests/t_geometric_program.py
+++ gpkit/tests/t_geometric_program.py
@@ -52,6 +52,38 @@ class t_GP(unittest.TestCase):
         for key in consenscheck:
             self.assertTrue(abs(1-sol["sensitivities"]["variables"][key]/consenscheck[key]) < 1e-2)
 
+    def test_simpleflight_nounits(self):
+        import simpleflight_nounits
+        gp = simpleflight_nounits.gp()
+        gp.solver = self.solver
+        sol = gp.solve()
+        freevarcheck = dict(A=8.46,
+                            C_D=0.0206,
+                            C_f=0.0036,
+                            C_L=0.499,
+                            Re=3.68e+06,
+                            S=16.4,
+                            W=7.34e+03,
+                            V=38.2,
+                            W_w=2.40e+03)
+        # sensitivity values from p. 34 of Woody"s thesis
+        consenscheck = {"(\\frac{S}{S_{wet}})": 0.4300,
+                        "e": -0.4785,
+                        "\\pi": -0.4785,
+                        "V_{min}": -0.3691,
+                        "k": 0.4300,
+                        "\mu": 0.0860,
+                        "(CDA0)": 0.0915,
+                        "C_{L,max}": -0.1845,
+                        "\\tau": -0.2903,
+                        "N_{ult}": 0.2903,
+                        "W_0": 1.0107,
+                        "\\rho": -0.2275}
+        for key in freevarcheck:
+            self.assertTrue(abs(1-sol["variables"][key]/freevarcheck[key]) < 1e-2)
+        for key in consenscheck:
+            self.assertTrue(abs(1-sol["sensitivities"]["variables"][key]/consenscheck[key]) < 1e-2)
+
 testcases = [t_GP]
 
 tests = []
