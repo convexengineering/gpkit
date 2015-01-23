f27249af18959c8ed52303210780f700347d4318
Warren Hoburg <whoburg@mit.edu>
2015-01-23 17:14:50 -0500

    remove matlab legacy code

--- matlab/convexfitting/repcols.m
+++
@@ -1,9 +0,0 @@
-function matout = repcols(matin, n)
-%replicate columns of a matrix
-%returns a matrix with n times as many columns as matin
-%example: if matin is [a b c], repcols(matin, 2) returns [a a b b c c].
-[nrow, ncol] = size(matin);
-
-matout = reshape(repmat(matin, n, 1), nrow, ncol*n);
-
-end
