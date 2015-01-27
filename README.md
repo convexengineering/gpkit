372c417fe5bc888668a15367fc04ae7add179287
Warren Hoburg <whoburg@mit.edu>
2015-01-27 12:49:43 -0500

    fix citing discrepancies

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ If you use GPkit, please cite it with the following bibtex:
 @Misc{gpkit,
     author = {MIT Convex Optimization Group},
     title = {GPkit},
-    howpublished = {\url{https://github.com/convexopt/gpkit}},
+    howpublished = {\url{http://github.com/convexopt/gpkit}},
     year = {2015},
     note = {Version 0.1}
     }
--- setup.py
+++ setup.py
@@ -34,9 +34,9 @@ If you use GPkit, please cite it with the following bibtex::
 @Misc{gpkit,
     author = {MIT Convex Optimization Group},
     title = {GPkit},
-    howpublished = {\url{https://github.com/convexopt/gpkit}},
+    howpublished = {\url{http://github.com/convexopt/gpkit}},
     year = {2015},
-    note = {Release 0.1}
+    note = {Version 0.1}
     }
 ```
 
