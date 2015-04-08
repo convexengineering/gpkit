d6f777f54f2d438e68e05fd6cdc9a4b99de3d27e
Warren Hoburg <whoburg@mit.edu>
2015-04-08 17:13:26 -0400

    get ready for CI, as suggested in #160

--- gpkit/tests/run_tests.py
+++ gpkit/tests/run_tests.py
@@ -25,18 +25,29 @@ tests += t_gp_solution_array.tests
 
 import gpkit
 
-def run():
+
+def run(xmloutput=False):
+
     suite = unittest.TestSuite()
     loader = unittest.TestLoader()
 
     for t in tests:
         suite.addTests(loader.loadTestsFromTestCase(t))
 
-    unittest.TextTestRunner(verbosity=2).run(suite)
-    print "\n######################################################################"
+    if xmloutput:
+        import xmlrunner
+        xmlrunner.XMLTestRunner(output='test_reports').run(suite)
+    else:
+        unittest.TextTestRunner(verbosity=2).run(suite)
+
+    print "\n#################################################################"
     print "Running with units disabled:"
     gpkit.disableUnits()
-    unittest.TextTestRunner(verbosity=1).run(suite)
+
+    if xmloutput:
+        xmlrunner.XMLTestRunner(output='test_reports_nounits').run(suite)
+    else:
+        unittest.TextTestRunner(verbosity=1).run(suite)
 
 if __name__ == '__main__':
     run()
