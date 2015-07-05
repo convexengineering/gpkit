b6caf004e6949ab7f3bdf483755ded3c8d888351
Warren Hoburg <whoburg@mit.edu>
2015-07-05 09:22:17 -0400

    fix and test taylor expansion mutability bug

--- gpkit/models/math.py
+++ gpkit/models/math.py
@@ -18,9 +18,9 @@ def te_exp_minus1(posy, nterm):
     """
     if nterm < 1:
         raise ValueError("Unexpected number of terms, nterm=%s" % nterm)
-    res = posy
+    res = 0
     factorial_denom = 1
-    for i in xrange(2, nterm + 1):
+    for i in xrange(1, nterm + 1):
         factorial_denom *= i
         res += posy**i / factorial_denom
     return res
--- gpkit/tests/t_models.py
+++ gpkit/tests/t_models.py
@@ -1,6 +1,6 @@
 """Tests for model library (gpkit/models/)"""
 import unittest
-from gpkit import Variable
+from gpkit import Variable, VectorVariable
 
 
 class TestMath(unittest.TestCase):
@@ -13,6 +13,15 @@ class TestMath(unittest.TestCase):
         self.assertEqual(te_exp_minus1(x, 1), x)
         self.assertEqual(te_exp_minus1(x, 3), x + x**2/2. + x**3/6.)
         self.assertRaises(ValueError, te_exp_minus1, x, 0)
+        # make sure x was not modified
+        self.assertEqual(x, Variable('x'))
+        # try for VectorVariable too
+        y = VectorVariable(3, 'y')
+        self.assertEqual(te_exp_minus1(y, 1), y)
+        self.assertEqual(te_exp_minus1(y, 3), y + y**2/2. + y**3/6.)
+        self.assertRaises(ValueError, te_exp_minus1, y, 0)
+        # make sure y was not modified
+        self.assertEqual(y, VectorVariable(3, 'y'))
 
 
 TESTS = [TestMath]
