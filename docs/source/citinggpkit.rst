3754fd8859acb943a18838ca8ef0a1c37c916ebb
Warren Hoburg <whoburg@mit.edu>
2015-07-24 18:29:41 -0400

    change group name for copyright

--- docs/source/citinggpkit.rst
+++ docs/source/citinggpkit.rst
@@ -4,7 +4,7 @@ Citing GPkit
 If you use GPkit, please cite it with the following bibtex::
 
     @Misc{gpkit,
-          author={MIT Convex Optimization Group},
+          author={MIT Department of Aeronautics and Astronautics},
           title={GPkit},
           howpublished={\url{https://github.com/convexopt/gpkit}},
           year={2015},
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'gpkit'
-copyright = u'2015, MIT Convex Optimization Group'
+copyright = u'2015, MIT Department of Aeronautics and Astronautics'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -207,7 +207,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   ('index', 'gpkit.tex', u'gpkit Documentation',
-   u'MIT Convex Optimization Group', 'manual'),
+   u'MIT Department of Aeronautics and Astronautics', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -237,7 +237,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'gpkit', u'gpkit Documentation',
-     [u'MIT Convex Optimization Group'], 1)
+     [u'MIT Department of Aeronautics and Astronautics'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -251,7 +251,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'gpkit', u'gpkit Documentation',
-   u'MIT Convex Optimization Group', 'gpkit', 'One line description of project.',
+   u'MIT Department of Aeronautics and Astronautics', 'gpkit', 'One line description of project.',
    'Miscellaneous'),
 ]
 
