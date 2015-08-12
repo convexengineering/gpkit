03f6847352e403731981be5bec49e905c3f17d6e
Edward Burnell <eburn@mit.edu>
2015-08-11 20:23:15 -0700

    make zero_lower_unbounded recursive

--- gpkit/tools.py
+++ gpkit/tools.py
@@ -7,10 +7,11 @@ from .variables import Variable, VectorVariable
 from .posyarray import PosyArray
 
 
-def zero_lower_unbounded(model):
-    bounds = model.gp(verbosity=0).missingbounds
-    zeros = {var: 0 for var, bound in bounds.items() if bound == "lower"}
-    model.substitutions.update(zeros)
+def zero_lower_unbounded(model, zeros=True):
+    while zeros:
+        bounds = model.gp(verbosity=0).missingbounds
+        zeros = {var: 0 for var, bound in bounds.items() if bound == "lower"}
+        model.substitutions.update(zeros)
 
 
 def te_exp_minus1(posy, nterm):
