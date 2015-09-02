b71da9e3e2f940ea505976384ab7c8ac14f199e2
pgkirsch <kirschen@mit.edu>
2015-09-02 11:15:25 -0400

    turns beam example into method and addresses code review comments

--- docs/source/examples/beam.py
+++ docs/source/examples/beam.py
@@ -1,46 +1,60 @@
-from gpkit.shortcuts import *
+"""
+A simple beam example with fixed geometry. Solves the discretized
+Euler-Bernoulli beam equations for a constant distributed load
+"""
 import numpy as np
 import matplotlib.pyplot as plt
+from gpkit.shortcuts import *
 
-N = 100
-L = 5.
-dx = L/(N-1)
-ei = 1E4
-EI = Var("EI", ei)
+def beam(N, L, EI, P):
 
-P = 100.
-p = Vec(N, "p", descr="Distributed load")
-p = p.sub(p, P*np.ones(N))
+    dx = L/(N-1)
+    EI = Var("EI", EI)
 
-V  = Vec(N, "V", label="Internal shear")
-M  = Vec(N, "M", label="Internal moment")
-th = Vec(N, "th", label="Slope")
-w  = Vec(N, "w", label="Displacement")
+    p = Vec(N, "p", label="Distributed load")
+    p = p.sub(p, P*np.ones(N))
 
-eps = 1E-16 #something small
+    V  = Vec(N, "V", label="Internal shear")
+    M  = Vec(N, "M", label="Internal moment")
+    th = Vec(N, "th", label="Slope")
+    w  = Vec(N, "w", label="Displacement")
 
-substitutions = {var: eps for var in [V[-1], M[-1], th[0], w[0]]}
+    eps = 1E-16 #an arbitrarily small positive number
 
-objective = w[-1]
+    substitutions = {var: eps for var in [V[-1], M[-1], th[0], w[0]]}
 
-constraints = [V.left[1:N]     >= V[1:N]    + 0.5*dx*(p.left[1:N]    + p[1:N]),
-               M.left[1:N]     >= M[1:N]    + 0.5*dx*(V.left[1:N]    + V[1:N]),
-               th.right[0:N-1] >= th[0:N-1] + 0.5*dx*(M.right[0:N-1] + M[0:N-1])/EI,
-               w.right[0:N-1]  >= w[0:N-1]  + 0.5*dx*(th.right[0:N-1]+ th[0:N-1])
-              ]
+    objective = w[-1]
 
-m = Model(objective, constraints, substitutions)
+    constraints = [V.left[1:N]     >= V[1:N]    + 0.5*dx*(p.left[1:N]    + p[1:N]),
+                   M.left[1:N]     >= M[1:N]    + 0.5*dx*(V.left[1:N]    + V[1:N]),
+                   th.right[0:N-1] >= th[0:N-1] + 0.5*dx*(M.right[0:N-1] + M[0:N-1])/EI,
+                   w.right[0:N-1]  >= w[0:N-1]  + 0.5*dx*(th.right[0:N-1]+ th[0:N-1])
+                  ]
 
-sol = m.solve(verbosity=1)
+    return Model(objective, constraints, substitutions)
 
-x = np.linspace(0, L, N)
-w_gp = sol("w")
-w_exact =  P/(24.*ei)* x**2 * (x**2  - 4*L*x + 6*L**2)
 
-assert max(abs(w_gp - w_exact)) <= 1e-4
+if __name__ == "__main__":
 
-plt.plot(x, w_gp, 'k', x, w_exact, 'b')
-plt.xlabel('x')
-plt.ylabel('Deflection')
-plt.axis('equal')
-plt.show()
+    PLOT = True
+
+    N = 10 #  [-] grid size
+    L = 5. #   [m] beam length
+    EI = 1E4 # [N*m^2] elastic modulus * area moment of inertia
+    P = 100 #  [N/m] magnitude of distributed load 
+
+    m = beam(N, L, EI, P)
+    sol = m.solve(verbosity=1)
+
+    x = np.linspace(0, L, N) # position along beam
+    w_gp = sol("w") # deflection along beam
+    w_exact =  P/(24.*EI)* x**2 * (x**2  - 4*L*x + 6*L**2) # analytical soln
+
+    assert max(abs(w_gp - w_exact)) <= 1e-4
+
+    if PLOT:
+        plt.plot(x, w_gp, 'k', x, w_exact, 'b')
+        plt.xlabel('x')
+        plt.ylabel('Deflection')
+        plt.axis('equal')
+        plt.show()
