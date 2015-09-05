4291196340293f511007034ce8b1a1dbff3ccd58
Edward Burnell <eburn@mit.edu>
2015-09-05 19:25:06 -0400

    rename testscripts

--- runtests.sh
+++ runtests.sh
@@ -1,1 +1,5 @@
-python -c "from gpkit.tests import run; run()"
+cp gpkit/env/settings .
+sed -i '1s/.*/installed_solvers : cvxopt/' gpkit/env/settings
+cat gpkit/env/settings
+python -c "import gpkit.tests; gpkit.tests.run()"
+mv settings gpkit/env
