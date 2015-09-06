cb4ea334e29f030abbd0b78d6da4a1bd7f0ac90a
Edward Burnell <eburn@mit.edu>
2015-09-06 18:35:59 -0400

    improve examples tests, document

--- gpkit/tests/t_examples.py
+++ gpkit/tests/t_examples.py
@@ -2,61 +2,56 @@ import unittest
 import numpy as np
 import sys
 import os
+import importlib
 
 from gpkit import settings
 
 FILE_DIR = os.path.dirname(os.path.realpath(__file__))
 EXAMPLE_DIR = os.path.abspath(FILE_DIR+'../../../docs/source/examples')
-WRITTEN = {}
-
-
-class capture_stdout(object):
-    def __init__(self, filename):
-        self.filename = EXAMPLE_DIR+os.sep+"%s_output.txt" % filename
-
-    def __enter__(self):
-        if self.filename not in WRITTEN:
-            self.original_stdout = sys.stdout
-            sys.stdout = open(self.filename, "w")
-
-    def __exit__(self, *args):
-        if self.filename not in WRITTEN:
-            sys.stdout.close()
-            sys.stdout = self.original_stdout
-            WRITTEN[self.filename] = True
+SOLVERS = settings["installed_solvers"]
+IMPORTED_EXAMPLES = {}
 
 
 class TestExamples(unittest.TestCase):
 
-    def test_simple_sp(self):
-        with capture_stdout("simple_sp"):
-            import simple_sp
+    # To test a new example, add a function called `test_$EXAMPLENAME`, where
+    # $EXAMPLENAME is the name of your example in docs/source/examples without
+    # the file extension.
+    #
+    # This function should accept two arguments (e.g. 'self' and 'example').
+    # The imported example script will be passed to the second: anything that
+    # was a global variable (e.g, "sol") in the original script is available
+    # as an attribute (e.g., "example.sol")
+    #
+    # If you don't want to perform any checks on the example besides making
+    # sure it runs, just put "pass" as the function's body, e.g.:
+    #
+    #       def test_dummy_example(self, example):
+    #           pass
+    #
+    # But it's good practice to ensure the example's solution as well, e.g.:
+    #
+    #       def test_dummy_example(self, example):
+    #           self.assertAlmostEqual(example.sol["cost"], 3.121)
+    #
 
-    def test_simple_box(self):
-        with capture_stdout("simple_box"):
-            import simple_box
+    def test_simple_sp(self, example):
+        pass
 
-    def test_x_greaterthan_1(self):
-        with capture_stdout("x_greaterthan_1"):
-            import x_greaterthan_1
+    def test_simple_box(self, example):
+        pass
 
-    def test_beam(self):
-        with capture_stdout("beam"):
-            import beam
+    def test_x_greaterthan_1(self, example):
+        pass
 
-    def test_water_tank(self):
-        with capture_stdout("water_tank"):
-            import water_tank
+    def test_beam(self, example):
+        pass
 
-    def test_simpleflight(self):
-        with capture_stdout("simpleflight"):
-            import simpleflight
-        m = simpleflight.m
-        # sweep with various solvers
-        m.solve(solver=self.solver, verbosity=0)
-        # check that the single solution is accurate
-        m.substitutions = {}
-        sol = m.solve(solver=self.solver, verbosity=0)
+    def test_water_tank(self, example):
+        pass
+
+    def test_simpleflight(self, example):
+        sol = example.sol
         freevarcheck = dict(A=8.46,
                             C_D=0.0206,
                             C_f=0.0036,
@@ -87,14 +82,71 @@ class TestExamples(unittest.TestCase):
             self.assertTrue(abs(1-sol_rat) < 1e-2)
 
 
+class default_solver(object):
+    def __init__(self, solver):
+        self.solver = solver
+
+    def __enter__(self):
+        import gpkit
+        gpkit.settings["installed_solvers"] = [self.solver]
+
+    def __exit__(self, *args):
+        import gpkit
+        gpkit.settings["installed_solvers"] = SOLVERS
+
+
+class nullFile(object):
+    def write(self, string):
+        pass
+
+    def close(self):
+        pass
+
+
+class capture_stdout(object):
+    def __init__(self, logfilename=None):
+        self.logfilename = logfilename
+
+    def __enter__(self):
+        self.original_stdout = sys.stdout
+        if self.logfilename:
+            filepath = EXAMPLE_DIR+os.sep+"%s_output.txt" % self.logfilename
+            logfile = open(filepath, "w")
+        else:
+            logfile = nullFile()
+        sys.stdout = logfile
+
+    def __exit__(self, *args):
+        sys.stdout.close()
+        sys.stdout = self.original_stdout
+
+
+def new_test(name, solver):
+    def test(self):
+        with default_solver(solver):
+            logfilename = name if name not in IMPORTED_EXAMPLES else None
+            with capture_stdout(logfilename):
+                if name not in IMPORTED_EXAMPLES:
+                    IMPORTED_EXAMPLES[name] = importlib.import_module(name)
+                else:
+                    reload(IMPORTED_EXAMPLES[name])
+                getattr(self, name)(IMPORTED_EXAMPLES[name])
+    return test
+
+
 TESTS = []
 if os.path.isdir(EXAMPLE_DIR):
     sys.path.insert(0, EXAMPLE_DIR)
-    for solver in settings["installed_solvers"]:
-        if solver:
-            test = type(TestExamples.__name__+"_"+solver, (TestExamples,), {})
-            setattr(test, "solver", solver)
-            TESTS.append(test)
+    for fn in dir(TestExamples):
+        if fn[:5] == "test_":
+            name = fn[5:]
+            old_test = getattr(TestExamples, fn)
+            setattr(TestExamples, name, old_test)  # move to a non-test fn
+            delattr(TestExamples, fn)  # delete the old old_test
+            for solver in SOLVERS:
+                new_name = "test_%s_%s" % (name, solver)
+                setattr(TestExamples, new_name, new_test(name, solver))
+    TESTS.append(TestExamples)
 
 if __name__ == "__main__":
     from gpkit.tests.helpers import run_tests
