396b722f2d4d5da4ccfdfe7349d2900211e96318
pgkirsch <kirschen@mit.edu>
2015-10-09 23:50:01 -0400

    ex5 close

--- gpkit/tests/Xu_examples/ex5.py
+++ gpkit/tests/Xu_examples/ex5.py
@@ -1,7 +1,6 @@
 '''
 Example 5, Xu 2013
-Status: Currently generates error (AttributeError: 'int' object has no attribute 'exps')
-NOTE: changing the objective to 1/x4 finds a solution
+Status: Getting close to right answer after many iterations
 '''
 
 from gpkit.shortcuts import *
@@ -21,16 +20,16 @@ k3 = 0.0391908
 k4 = 0.9*k3
 
 with SignomialsEnabled():
-    m = Model(t, # slightly different
-             [t >= -x4,
-              x1 + k1*x1*x5 <= 1,
+    m = Model(1/x4, # different because GPkit doesn't support negative objectives
+             [
+#              x1 + k1*x1*x5 <= 1,
               x1 + k1*x1*x5 >= 1,
               x2 + k2*x2*x6 >= x1,
-              x2 + k2*x2*x6 <= x1,
-              x3 + x1 + k3*x3*x5 >= 1,
+#              x2 + k2*x2*x6 <= x1,
+#              x3 + x1 + k3*x3*x5 >= 1,
               x3 + x1 + k3*x3*x5 <= 1,
               x4 + x2 + k4*x4*x6 <= x1 + x3,
-              x4 + x2 + k4*x4*x6 >= x1 + x3,
+#              x4 + x2 + k4*x4*x6 >= x1 + x3,
               x5**0.5 + x6**0.5 <= 4,
               x5 >= 1E-5,
               x6 >= 1E-5,
@@ -42,5 +41,5 @@ with SignomialsEnabled():
               x6 <= 16
               ])
 
-m.localsolve()
+sol = m.localsolve(rel_tol=1E-12, iteration_limit=100)
 
