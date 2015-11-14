3fa0c3e5d8e050d5802a1c3048220b078940b79c
Edward Burnell <eburn@mit.edu>
2015-11-14 10:11:08 -0500

    remove DYLIB and changes to bash profile

--- gpkit/build.py
+++ gpkit/build.py
@@ -185,14 +185,6 @@ class Mosek(SolverBackend):
         global settings
         settings["mosek_bin_dir"] = self.bin_dir
         os.environ['PATH'] = os.environ['PATH']+':%s' % self.bin_dir
-        if sys.platform == "darwin":
-            prev = os.environ.get('DYLD_LIBRARY_PATH', "")
-            os.environ['DYLD_LIBRARY_PATH'] = prev +':%s' % self.bin_dir
-            prof_str = "export DYLD_LIBRARY_PATH=\$DYLD_LIBRARY_PATH:" + self.bin_dir
-            call(('if ! grep "^%s" $HOME/.bash_profile;'
-                  ' then echo "%s" >> $HOME/.bash_profile;'
-                  ' fi') % (prof_str, prof_str))
-            call("cat $HOME/.bash_profile")
 
         return "version %s, installed to %s" % (self.version, self.dir)
 
