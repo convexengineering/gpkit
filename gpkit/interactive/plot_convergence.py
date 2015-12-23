24ec097ae90b67fb3f459f1b13066aa1ba8b991c
Warren Hoburg <whoburg@mit.edu>
2015-12-23 16:39:58 -0500

    pep8, docstring tweaks, return a figure

--- gpkit/interactive/plot_convergence.py
+++ gpkit/interactive/plot_convergence.py
@@ -3,35 +3,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_convergence(m):
+def plot_convergence(model):
     """Plots the convergence of a signomial programming model
-    
+
     Arguments
     ---------
-    m: Signomial programming model that has already been solved
-    
+    model: Model
+        Signomial programming model that has already been solved
+
     Returns
     -------
-    Doesn't return anything, but produces a plot
+    matplotlib.pyplot Figure
+        Semilogy plot of variable values as functions of SP iteration #
     """
-
-    keyList = m.program.gps[0].result['variables'].keys()
-
     newDict = {}
 
-    plt.figure()
-    ax = plt.subplot(111)
+    fig, ax = plt.subplots()
 
-    for k in keyList:
+    for key in model.program.gps[0].result['variables']:
         a = np.array([])
-        for j in range(len(m.program.gps)):
-            a = np.append(a, m.program.gps[j].result['variables'][k])
-        newDict[k] = a
-        plt.semilogy(np.arange(len(newDict[k])), newDict[k], label=k.name)
+        for j in range(len(model.program.gps)):
+            a = np.append(a, model.program.gps[j].result['variables'][key])
+        newDict[key] = a
+        ax.semilogy(np.arange(len(newDict[key])), newDict[key], label=key.name)
 
     box = ax.get_position()
     ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-    plt.xlabel('Number of iterations')
-    plt.ylabel('Normalised variable values')
+    ax.set_xlabel('Number of iterations')
+    ax.set_ylabel('Normalized variable values')
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    plt.show()
+    return fig
