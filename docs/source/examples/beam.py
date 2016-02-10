convexengineering/gpkit

commit 5473d92f87fa8bbe39fa125f8a8dd8e7ece7464a

scattered pylint fixes

docs/source/examples/beam.py
@@ -3,7 +3,7 @@ A simple beam example with fixed geometry. Solves the discretized
 Euler-Bernoulli beam equations for a constant distributed load
 """
 import numpy as np
-from gpkit.shortcuts import *
+from gpkit.shortcuts import Var, Model
 
 
 class Beam(Model):
@@ -52,9 +52,8 @@ class Beam(Model):
 b = Beam(N=10, substitutions={"L": 6, "EI": 1.1e4, "q": 110*np.ones(10)})
 sol = b.solve(verbosity=1)
 L, EI, q = sol("L"), sol("EI"), sol("q")
-N = len(q)
 q = q[0]  # assume uniform loading for the check below
-x = np.linspace(0, L, N)  # position along beam
+x = np.linspace(0, L, len(q))  # position along beam
 w_gp = sol("w")  # deflection along beam
 w_exact = q/(24.*EI) * x**2 * (x**2 - 4*L*x + 6*L**2)  # analytic soln
 

docs/source/examples/water_tank.py
@@ -1,9 +1,9 @@
 from gpkit import Variable, VectorVariable, Model
-M   = Variable("M", 100, "kg", "Mass of Water in the Tank")
+M = Variable("M", 100, "kg", "Mass of Water in the Tank")
 rho = Variable("\\rho", 1000, "kg/m^3", "Density of Water in the Tank")
-A   = Variable("A", "m^2", "Surface Area of the Tank")
-V   = Variable("V", "m^3", "Volume of the Tank")
-d   = VectorVariable(3, "d", "m", "Dimension Vector")
+A = Variable("A", "m^2", "Surface Area of the Tank")
+V = Variable("V", "m^3", "Volume of the Tank")
+d = VectorVariable(3, "d", "m", "Dimension Vector")
 
 constraints = (A >= 2*(d[0]*d[1] + d[0]*d[2] + d[1]*d[2]),
                V == d[0]*d[1]*d[2],

commit 341633e26df47ab15e2834c2ac680f896d217148

fix errors introduced by 5473d92f87fa8bbe39fa125f8a8dd8e7ece7464a

docs/source/examples/beam.py
@@ -3,7 +3,7 @@ A simple beam example with fixed geometry. Solves the discretized
 Euler-Bernoulli beam equations for a constant distributed load
 """
 import numpy as np
-from gpkit.shortcuts import Var, Model
+from gpkit.shortcuts import Var, Vec, Model
 
 
 class Beam(Model):
@@ -52,8 +52,8 @@ class Beam(Model):
 b = Beam(N=10, substitutions={"L": 6, "EI": 1.1e4, "q": 110*np.ones(10)})
 sol = b.solve(verbosity=1)
 L, EI, q = sol("L"), sol("EI"), sol("q")
-q = q[0]  # assume uniform loading for the check below
 x = np.linspace(0, L, len(q))  # position along beam
+q = q[0]  # assume uniform loading for the check below
 w_gp = sol("w")  # deflection along beam
 w_exact = q/(24.*EI) * x**2 * (x**2 - 4*L*x + 6*L**2)  # analytic soln
 