736adc22c09d5c44992c7024d5cafc06dca89790
Warren Hoburg <whoburg@mit.edu>
2016-02-29 18:09:45 -0500

    convert tabs to spaces

--- gpkit/breakdown.py
+++ gpkit/breakdown.py
@@ -5,81 +5,81 @@ from gpkit.shortcuts import *
 import numpy.testing as npt
 
 class Breakdown(Model):
-	def __init__(self, input):
-		#create the list of variables to make constraints out of
-		self.constr=[]
-		self.varlist=[]
-		#call recursive function to create gp constraints
-		total=self.recurse(input)
+    def __init__(self, input):
+        #create the list of variables to make constraints out of
+        self.constr=[]
+        self.varlist=[]
+        #call recursive function to create gp constraints
+        total=self.recurse(input)
 
-		self.sol=self.solve_method()
+        self.sol=self.solve_method()
 
         
         def getSolution(self):
                 return self.sol
             
-	def solve_method(self):
-		m=Model(self.make_objective(),self.make_constraints())
-		return m.solve(verbosity=0)
-	
-	def recurse(self,input):
-		order=input.keys()
-		i=0
-		hold=[]
-		while i<len(order):
-			if isinstance(input[order[i]],dict):
-				#create the variable
-				var=Var(order[i],None)
-				self.varlist.append(var)
-				#need to recurse again
-				vars=self.recurse(input[order[i]])
-				j=0
-				varhold=0
-				while j<len(vars):
-					varhold=varhold+vars[j]
-					j=j+1
-				hold.append(var)
-				self.constr.append([var,vars])
-			elif isinstance(input[order[i]],list):
-				#need to create a var of name dict entry
-				if len(input[order[i]])==1:
-					var=Var(order[i],input[order[i]][0])
-				elif len(input[order[i]])==2:
-					var=Var(order[i],input[order[i]][0],input[order[i]][1])
-				elif len(input[order[i]])==3:
-					var=Var(order[i],input[order[i]][0],input[order[i]][1],input[order[i]][1])
-				elif len(input[order[i]])==4:
-					var=Var(order[i],input[order[i]][1],input[order[i]][2],input[order[i]][3],input[order[i]][4])
-					
-				self.varlist.append(var)
-				hold.append(var)
-			else:
-				#create a var
-				var=Var(order[i],input[order[i]])
-				self.varlist.append(var)
-				hold.append(var)
-			i=i+1
-		return hold
-		
-	#method to generate the gp constraints
-	def make_constraints(self):
-		i=0
-		constraints=[]
-		while i<len(self.constr):
-			j=0
-			sum=0
-			while j<len(self.constr[i][1]):
-				sum=sum+self.constr[i][1][j]
-				j=j+1
-			constraints.append(1>=sum/self.constr[i][0])
-			i=i+1
-		return constraints
-		
-	def make_objective(self):
-		#return the first variable that is created, this is what should be minimized
-		return self.varlist[0]
-	
-	def test(self):
+    def solve_method(self):
+        m=Model(self.make_objective(),self.make_constraints())
+        return m.solve(verbosity=0)
+    
+    def recurse(self,input):
+        order=input.keys()
+        i=0
+        hold=[]
+        while i<len(order):
+            if isinstance(input[order[i]],dict):
+                #create the variable
+                var=Var(order[i],None)
+                self.varlist.append(var)
+                #need to recurse again
+                vars=self.recurse(input[order[i]])
+                j=0
+                varhold=0
+                while j<len(vars):
+                    varhold=varhold+vars[j]
+                    j=j+1
+                hold.append(var)
+                self.constr.append([var,vars])
+            elif isinstance(input[order[i]],list):
+                #need to create a var of name dict entry
+                if len(input[order[i]])==1:
+                    var=Var(order[i],input[order[i]][0])
+                elif len(input[order[i]])==2:
+                    var=Var(order[i],input[order[i]][0],input[order[i]][1])
+                elif len(input[order[i]])==3:
+                    var=Var(order[i],input[order[i]][0],input[order[i]][1],input[order[i]][1])
+                elif len(input[order[i]])==4:
+                    var=Var(order[i],input[order[i]][1],input[order[i]][2],input[order[i]][3],input[order[i]][4])
+                    
+                self.varlist.append(var)
+                hold.append(var)
+            else:
+                #create a var
+                var=Var(order[i],input[order[i]])
+                self.varlist.append(var)
+                hold.append(var)
+            i=i+1
+        return hold
+        
+    #method to generate the gp constraints
+    def make_constraints(self):
+        i=0
+        constraints=[]
+        while i<len(self.constr):
+            j=0
+            sum=0
+            while j<len(self.constr[i][1]):
+                sum=sum+self.constr[i][1][j]
+                j=j+1
+            constraints.append(1>=sum/self.constr[i][0])
+            i=i+1
+        return constraints
+        
+    def make_objective(self):
+        #return the first variable that is created, this is what should be minimized
+        return self.varlist[0]
+    
+    def test(self):
             npt.assert_almost_equal(self.sol('w'),15, decimal=5)
             npt.assert_almost_equal(self.sol('w1'),11, decimal=5)
             npt.assert_almost_equal(self.sol('w2'),3, decimal=5)
@@ -90,17 +90,8 @@ class Breakdown(Model):
             npt.assert_almost_equal(self.sol('w122'),6, decimal=5)
             npt.assert_almost_equal(self.sol('w21'),1, decimal=5)
             npt.assert_almost_equal(self.sol('w22'),2, decimal=5)
-		
+        
 if __name__=="__main__":
         test={'w':{'w1':{'w11':[3],'w12':{'w121':[2],'w122':[6]}},'w2':{'w21':[1],'w22':[2]},'w3':[1]}}
-	bd=Breakdown(test)
+    bd=Breakdown(test)
         bd.test()
-	
-		
-		
-	
-		
-		
-		
-		
-		
