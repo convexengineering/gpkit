3892e96145e4de494ef112c6bf65945e9c36fa66
Edward Burnell <eburn@mit.edu>
2016-03-10 03:22:50 -0500

    add docstring

--- gpkit/constraints/array.py
+++ gpkit/constraints/array.py
@@ -4,7 +4,12 @@ from .single_equation import SingleEquationConstraint
 
 
 class ArrayConstraint(ConstraintSet, SingleEquationConstraint):
-    "A ConstraintSet for prettier array-constraint printing"
+    """A ConstraintSet for prettier array-constraint printing.
+
+    Because ArrayConstraint also inherits from ConstraintSet,
+    its substitutions is recursive and so left and right are
+    only used for string and latex printing. In practice they're
+    likely to be either NomialArrays or Varkeys of VectorVariables."""
     def __init__(self, constraints, left, oper, right):
         super(ArrayConstraint, self).__init__(constraints)
         self.left = left
