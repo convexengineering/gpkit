445bc84139a94901e408f988368976c849f8d2ae
pgkirsch <kirschen@mit.edu>
2016-03-17 16:10:50 -0400

    Tight Constraint Set

---
+++ gpkit/constraints/tight.py
@@ -0,0 +1,25 @@
+from .set import ConstraintSet
+from ..nomials import PosynomialInequality, SignomialInequality
+
+class TightConstraintSet(ConstraintSet):
+    """A constraint set for which the inequality constraints must be
+    satisfied tightly"""
+
+    def process_result(self, result):
+        """Checks that all constraints are satisfied tightly"""
+        ConstraintSet.process_result(self, result)
+        variables = result["variables"]
+        for constraint in self.flat():
+            if isinstance(constraint, (PosynomialInequality,
+                                       SignomialInequality)):
+                leftsubbed = constraint.left.sub(variables)
+                rightsubbed = constraint.right.sub(variables)
+                if leftsubbed != rightsubbed:
+                    raise ValueError("Tightness requirement not met"
+                                     " for constraint %s because %s"
+                                     " evaluated to %s but %s evaluated"
+                                     " to %s"
+                                     % (constraint,
+                                        constraint.left, leftsubbed,
+                                        constraint.right, rightsubbed))
+
