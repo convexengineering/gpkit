fix sweep with united subs, printing of 'count' units

gpkit/small_classes.py
@@ -141,10 +141,13 @@ def _enray_and_unit_dict(d_in, d_out, unitless_keys=(), united=False):
         else:
             if len(v) == 1:
                 v = v[0]
-            v = np.array(v)
-            if (united and hasattr(k, "units")
-                    and isinstance(k.units, Quantity)):
-                v = v*k.units
+            elif hasattr(v[0], "units"):
+                v = [e.to(v[0].units).magnitude for e in v]*v[0].units
+            else:
+                v = np.array(v)
+                if (united and hasattr(k, "units")
+                        and isinstance(k.units, Quantity)):
+                    v = v*k.units
             d_out[k] = v
     # assert set(i.keys()) == set(o.keys())  # keys change with swept varkeys
     return d_out