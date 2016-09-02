2207a8d9a441c82e2dcdfc3196ff856989035a04
Edward Burnell <eburn@mit.edu>
2016-09-02 12:04:49 -0700

    make unbounded example that works for cvxopt

--- docs/source/examples/unbounded.py
+++ docs/source/examples/unbounded.py
@@ -1,25 +1,14 @@
+"Demonstrate a trivial unbounded variable"
 from gpkit import Variable, Model, settings
 from gpkit.constraints.bounded import BoundedConstraintSet
 
-A = Variable("A")
-D = Variable("D")
-F = Variable("F")
-mi = Variable("m_i")
+x = Variable("x")
 
-Fs = Variable("Fs", 0.9)
-mb = Variable("m_b", 0.4)
-V = Variable("V", 300)
+constraints = [x <= 1]
 
-constraints = [F >= D + V**2,
-               D == V**2*A,
-               V >= mi + mb,
-               Fs <= mi,
-               ]
-
-# Model(F, constraints).solve("mosek")  # does not solve
-if "mosek" in settings["installed_solvers"]:
-    m = Model(F, BoundedConstraintSet(constraints))
-    # by default, prints bounds warning during solve
-    sol = m.solve("mosek", verbosity=0)
-    # bound waring is also accessible in sol:
-    print sol["boundedness"]
+# Model(x, constraints).solve()  # does not solve
+m = Model(x, BoundedConstraintSet(constraints))
+# by default, prints bounds warning during solve
+sol = m.solve(verbosity=0)
+# bound waring is available in sol["boundedness"]:
+print sol["boundedness"]
