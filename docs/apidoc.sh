2644cd3d2586b2d680584933ec568f5d6cbe9438
Edward Burnell <eburn@mit.edu>
2016-09-05 09:03:11 -0700

    work on #752

--- docs/apidoc.sh
+++ docs/apidoc.sh
@@ -1,3 +1,6 @@
 rm source/autodoc/*
 sphinx-apidoc ../gpkit -o source/autodoc
-sed -i '1i Glossary\n********\n\n*For an alphabetical listing of all commands, check out the* :ref:`genindex`\n' source/autodoc/gpkit.rst
+
+sed -i '1,3d' source/autodoc/gpkit.rst
+header="Glossary\n********\n\n*For an alphabetical listing of all commands, check out the* :ref:\`genindex\`\n"
+sed -i "1i $header" source/autodoc/gpkit.rst
