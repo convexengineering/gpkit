4d3e21b319157dbc10d4132c7d483fe792ccef2e
Marshall Galbraith <galbramc@mit.edu>
2016-09-05 14:05:16 -0400

    Fixed pyint in interactive.ractor

--- gpkit/interactive/ractor.py
+++ gpkit/interactive/ractor.py
@@ -39,14 +39,25 @@ def ractorjs(title, model, update_py, ranges, constraint_js=""):
     widget_id = "jswidget_"+str(JSWIDGET_ID())
     display(HTML("<script id='%s-after' type='text/throwaway'>%s</script>" %
                  (widget_id, constraint_js)))
-    display(HTML("<script>var %s = {storage: [], n:%i, ranges: {}, after: document.getElementById('%s-after').innerHTML, bases: [1] }</script>" % (widget_id, len(ranges), widget_id)))
+    display(HTML(
+        ("<script>var %s = {storage: [], n:%i, ranges: {}, after: "
+         "document.getElementById('%s-after').innerHTML, bases: [1] }"
+         "</script>") %
+        (widget_id, len(ranges), widget_id)))
 
     container_id = widget_id + "_container"
-    display(HTML("<div id='%s'></div><style>#%s td {text-align: right; border: none !important;}\n#%s tr {border: none !important;}\n#%s table {border: none !important;}\n</style>" % (container_id, container_id, container_id, container_id)))
+    display(HTML(
+        ("<div id='%s'></div><style>#%s td "
+         "{text-align: right; border: none !important;}\n"
+         "#%s tr {border: none !important;}\n"
+         "#%s table {border: none !important;}\n</style>") %
+        (container_id, container_id, container_id, container_id)))
 
     template_id = widget_id + "_template"
     template = '<script id="%s" type="text/ractive"><table>' % template_id
-    ctrl_template = Template('<tr><td>$var</td><input value="{{$varname}}" type="range" min="0" max="$len" step="1"><td><span id="$w-$varname"></span></td></tr>\n')
+    ctrl_template = Template(('<tr><td>$var</td><input value="{{$varname}}" '
+                              'type="range" min="0" max="$len" step="1"><td>'
+                              '<span id="$w-$varname"></span></td></tr>\n'))
     data_init = ""
 
     subs = {}
@@ -94,7 +105,9 @@ def ractorjs(title, model, update_py, ranges, constraint_js=""):
 
     display(HTML(template + "</table></script>"))
 
-    loader = Template("""getScript('http://cdn.ractivejs.org/0.6.1/ractive.min.js', function() {
+    loader = Template(
+        """getScript('http://cdn.ractivejs.org/0.6.1/ractive.min.js',
+          function() {
           $w.ractive = new Ractive({
           el: '$container_id',
           template: '#$template_id',
@@ -106,7 +119,8 @@ def ractorjs(title, model, update_py, ranges, constraint_js=""):
                   varname = 'var'+i
                   idx = $w.ractive.data[varname]
                   console.log("$w-"+varname);
-                  document.getElementById("$w-"+varname).innerText = Math.round(100*$w.ranges[varname][idx])/100
+                  document.getElementById("$w-"+varname).innerText
+                     = Math.round(100*$w.ranges[varname][idx])/100
                   idxsum += idx*$w.bases[i]
               }
               if ($w.storage[idxsum] === "") {
