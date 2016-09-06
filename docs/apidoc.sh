aba0aa99cf5a2bf637480e7649f662c9abfb7633
Marshall Galbraith <galbramc@mit.edu>
2016-09-06 18:12:30 -0400

    Replaced sed command with cat command

--- docs/apidoc.sh
+++ docs/apidoc.sh
@@ -1,6 +1,9 @@
 rm source/autodoc/*
 sphinx-apidoc ../gpkit -o source/autodoc
 
-sed -i '1,3d' source/autodoc/gpkit.rst
+# Delete fist 3 lines
+sed -i'' -e '1,3d' source/autodoc/gpkit.rst
+
+# Add header
 header="Glossary\n********\n\n*For an alphabetical listing of all commands, check out the* :ref:\`genindex\`\n"
-sed -i "1i $header" source/autodoc/gpkit.rst
+echo $header | cat - source/autodoc/gpkit.rst > gpkit.rst && mv gpkit.rst source/autodoc/gpkit.rst
