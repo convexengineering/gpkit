56b1e249a87d889e37c05771e1f145bf4680edaa
Warren Hoburg <whoburg@mit.edu>
2016-10-13 15:57:12 -0400

    better names; dynamic() methods

--- performance_modeling_test.py
+++ performance_modeling_test.py
@@ -2,13 +2,15 @@
 import numpy as np
 from gpkit import Model, Variable, vectorize
 
-# pylint: disable=invalid-name
-
-
 class Aircraft(Model):
     "the JHO vehicle"
+
+    def dynamic(self, state):
+        """Creates an instance of this component's performance model,
+        given a state"""
+        return AircraftP(self, state)
+
     def __init__(self, **kwargs):
-        self.dynamic_model = AircraftP
         self.fuse = Fuselage()
         self.wing = Wing()
 
@@ -21,13 +23,20 @@ class Aircraft(Model):
 
 
 class AircraftP(Model):
-    def __init__(self, static, state, **kwargs):
-        self.wing = static.wing.dynamic_model(static.wing, state)
-        Model.__init__(self, None, [self.wing], **kwargs)
+    """Aircraft Performance Model (i.e., aircraft flight physics)
+    Currently implemented as lift = weight
+    """
+    def __init__(self, aircraft, state, **kwargs):
+        self.aircraft = aircraft
+        self.wing_aero = aircraft.wing.dynamic(state)
+        csr = [aircraft["W_aircraft"] <= (0.5*state["\\rho"]*state["V"]**2
+                                          * self.wing_aero["C_L"]
+                                          * aircraft.wing["S"])]
+        Model.__init__(self, None, [csr, self.wing_aero], **kwargs)
 
 
 class FlightState(Model):
-    "One chunk of a mission"
+    "Instantaneous context for evaluating flight physics"
     def __init__(self, **kwargs):
         V = Variable("V", 40, "knots", "true airspeed")
         mu = Variable("\\mu", 1.628e-5, "N*s/m^2", "dynamic viscosity")
@@ -39,52 +48,56 @@ class FlightState(Model):
 
 
 class FlightSegment(Model):
-    def __init__(self, N, aircraft, **kwargs):
-        with vectorize(N):
-            fs = FlightState()
-            aircraftP = aircraft.dynamic_model(aircraft, fs)
-            csr = [aircraft["W_aircraft"] <= (0.5*fs["\\rho"]*fs["V"]**2
-                                              * aircraftP.wing["C_L"]
-                                              * aircraft.wing["S"])]
-        Model.__init__(self, aircraftP.wing["C_D"].prod()**(1./N),
-                       [fs, aircraft, aircraftP, csr], **kwargs)
+    "Combines a context (flight state) and a component (the aircraft)"
+    def __init__(self, aircraft, **kwargs):
+        self.flightstate = FlightState()
+        self.aircraftp = aircraft.dynamic(self.flightstate)
+        Model.__init__(self, None,
+                       [self.flightstate, self.aircraftp], **kwargs)
 
 
 class Wing(Model):
     "The thing that creates the lift"
+    def dynamic(self, state):
+        """Creates an instance of this component's performance model,
+        given a state"""
+        return WingAero(self, state)
+
     def __init__(self, **kwargs):
         W = Variable("W", "lbf", "weight")
         S = Variable("S", 190, "ft^2", "surface area")
         rho = Variable("\\rho", 1, "lbf/ft^2", "areal density")
         A = Variable("A", 27, "-", "aspect ratio")
         c = Variable("c", "ft", "mean chord")
-        self.dynamic_model = WingP
 
         csr = [W >= S*rho,
                c == (S/A)**0.5]
         super(Wing, self).__init__(None, csr, **kwargs)
 
 
-class WingP(Model):
-    def __init__(self, static, state, **kwargs):
+class WingAero(Model):
+    "Wing drag model"
+    def __init__(self, wing, state, **kwargs):
         CD = Variable("C_D", "-", "drag coefficient")
         CL = Variable("C_L", "-", "lift coefficient")
         e = Variable("e", 0.9, "-", "Oswald efficiency")
         Re = Variable("Re", "-", "Reynold's number")
-        csr = [CD >= (0.074/Re**0.2 + CL**2/np.pi/static["A"]/e),
-               Re == state["\\rho"]*state["V"]*static["c"]/state["\\mu"],
-               ]
+        csr = [CD >= (0.074/Re**0.2 + CL**2/np.pi/wing["A"]/e),
+               Re == state["\\rho"]*state["V"]*wing["c"]/state["\\mu"],
+              ]
         Model.__init__(self, None, csr, **kwargs)
 
 
 class Fuselage(Model):
     "The thing that carries the fuel, engine, and payload"
     def __init__(self, **kwargs):
-        V = Variable("V", 16, "gal", "volume")
-        d = Variable("d", 12, "in", "diameter")
+        # fuselage needs an external dynamic drag model,
+        # left as an exercise for the reader
+        # V = Variable("V", 16, "gal", "volume")
+        # d = Variable("d", 12, "in", "diameter")
         # S = Variable("S", "ft^2", "wetted area")
-        cd = Variable("c_d", .0047, "-", "drag coefficient")
-        CDA = Variable("CDA", "ft^2", "drag area")
+        # cd = Variable("c_d", .0047, "-", "drag coefficient")
+        # CDA = Variable("CDA", "ft^2", "drag area")
         W = Variable("W", 100, "lbf", "weight")
 
         csr = [  # CDA >= cd*4*V/d,
@@ -97,8 +110,9 @@ class Fuselage(Model):
 if __name__ == "__main__":
     JHO = Aircraft()
     N = 4
-    JHO = FlightSegment(N, JHO)
-    JHO.substitutions["V"] = np.linspace(20, 40, N)
-    # JHO.debug(solver="mosek")
-    SOL = JHO.solve("mosek")
+    with vectorize(N):
+        FS = FlightSegment(JHO)
+    COST = FS.aircraftp.wing_aero["C_D"].prod()**(1./N)
+    M = Model(COST, [FS, JHO], {"V": np.linspace(20, 40, N)})
+    SOL = M.solve("mosek")
     print SOL.table()
