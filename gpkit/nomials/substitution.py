1131aec1e6e7a693c07696bebfc0f880240a3556
Edward Burnell <eburn@mit.edu>
2016-10-24 20:53:31 -0400

    close #929

--- gpkit/nomials/substitution.py
+++ gpkit/nomials/substitution.py
@@ -125,7 +125,7 @@ def substitution(nomial, substitutions):
             varlocs_[var].remove(i)
             if len(varlocs_[var]) == 0:
                 del varlocs_[var]
-            if isinstance(sub, Numbers):
+            if isinstance(sub, (Numbers, np.ndarray)):
                 if getattr(sub, "shape", False):
                     raise ValueError("cannot substitute array %s for variable %s"
                                      % (sub, var))
