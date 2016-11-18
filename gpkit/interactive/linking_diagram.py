78726a0959f4b5358de38ca915cccb796aa31810
Edward Burnell <eburn@mit.edu>
2016-11-18 18:37:56 -0500

    pylint

--- gpkit/interactive/linking_diagram.py
+++ gpkit/interactive/linking_diagram.py
@@ -1,7 +1,10 @@
-from math import sin, cos, pi
+"Module for creating diagrams illustrating variables shared between submodels."
 from itertools import combinations
+from numpy import sin, cos, pi
 
-def make_figure(topmodel, subsystems, filename):
+
+# pylint: disable=too-many-locals
+def linking_diagram(topmodel, subsystems, filename):
     """
     Method to create a latex diagram illustrating how variables
     are linked between a parent model and its submodels
@@ -13,16 +16,16 @@ def make_figure(topmodel, subsystems, filename):
     the topmodel
     filename - a string which is the name of the file latex output
     will be written to
-    
+
     note: the following packages must be used in the latex file
-    \usepackage{tikz}
-    \usetikzlibrary{backgrounds}
+    \\usepackage{tikz}
+    \\usetikzlibrary{backgrounds}
     """
     keychain = {}
     for vk in topmodel.varkeys:
         model = vk.descr.get("models")
         if model == [topmodel.name] and vk not in topmodel.substitutions:
-            v = vk.str_without(['models'])
+            v = vk.str_without(["models"])
             keychain[v] = []
             for ss in subsystems:
                 if v in ss.varkeys:
@@ -48,10 +51,10 @@ def make_figure(topmodel, subsystems, filename):
     # Get rid of any empty entries
     viera = {k: v for k, v in zidane.items() if v}
 
-    with open(filename, 'w') as outfile:
+    with open(filename, "w") as outfile:
 
-        outfile.write('\\begin{center}\n' +
-                      '\\begin{tikzpicture}[thick]\n')
+        outfile.write("\\begin{center}\n" +
+                      "\\begin{tikzpicture}[thick]\n")
 
         # Create a circular ring of nodes, one for each model
         nodepos = {}
@@ -60,14 +63,14 @@ def make_figure(topmodel, subsystems, filename):
         R = 6
         for model in modellist:
             nodepos[model] = (R*sin(2*pi*i/I), R*cos(2*pi*i/I))
-            outfile.write('\\node at {0}'.format(str(nodepos[model])) +
-                          '[circle, minimum size=3cm, fill=blue!20]' +
-                          '({0}){{{0}}};\n'.format(model))
+            outfile.write("\\node at {0}".format(str(nodepos[model])) +
+                          "[circle, minimum size=3cm, fill=blue!20]" +
+                          "({0}){{{0}}};\n".format(model))
             i = i + 1
 
         j = 0
-        colours = ['red', 'blue', 'cyan ', 'magenta', 'brown', 'gray', 
-                   'olive', 'orange']
+        colours = ["red", "blue", "cyan ", "magenta", "brown", "gray",
+                   "olive", "orange"]
         for key in viera:
             # Create a node for every group of variables
             vargroup = viera[key]
@@ -78,20 +81,20 @@ def make_figure(topmodel, subsystems, filename):
                 varnodeposy += nodepos[k][1]
             varnodeposx = varnodeposx/len(key)
             varnodeposy = varnodeposy/len(key)
-            outfile.write('\\node at (%.2f,%.2f)' % (varnodeposx, varnodeposy) +
-                          '[draw,rectangle, color=%s, align=left, fill=white]({'
+            outfile.write("\\node at (%.2f,%.2f)" % (varnodeposx, varnodeposy) +
+                          "[draw,rectangle, color=%s, align=left, fill=white]({"
                           % colours[j] +
-                          ' '.join(vargroup).replace('\\','') + '}){$' +
-                          '$\\\\$'.join(vargroup) + '$};\n')
-            # Create edges from vargroups to models they appear in 
+                          " ".join(vargroup).replace("\\", "") + "}){$" +
+                          "$\\\\$".join(vargroup) + "$};\n")
+            # Create edges from vargroups to models they appear in
             for k in key:
-                outfile.write('\\begin{pgfonlayer}{background}\n' +
-                              '\\draw[color=%s] ({' % colours[j] +
-                              ' '.join(vargroup).replace('\\','') +
-                              '})--('+k+');\n' +
-                              '\\end{pgfonlayer}\n')
+                outfile.write("\\begin{pgfonlayer}{background}\n" +
+                              "\\draw[color=%s] ({" % colours[j] +
+                              " ".join(vargroup).replace("\\", "") +
+                              "})--("+k+");\n" +
+                              "\\end{pgfonlayer}\n")
 
             j += 1
 
-        outfile.write('\\end{tikzpicture}\n' +
-                      '\\end{center}')
+        outfile.write("\\end{tikzpicture}\n" +
+                      "\\end{center}")
