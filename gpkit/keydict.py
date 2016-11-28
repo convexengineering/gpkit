047d40a49782ce8b59cd953d34f658e9115aecc3
Edward Burnell <eburn@mit.edu>
2016-11-28 11:30:21 -0500

    fix solution array bugs

--- gpkit/keydict.py
+++ gpkit/keydict.py
@@ -5,15 +5,15 @@ from .small_classes import Numbers
 
 
 class KeyDict(dict):
-    """KeyDicts allow storing and accessing the same value with multiple keys
-
-    A KeyDict keeps an internal list of VarKeys as canonical keys, but allows
-    accessing their values with any object whose `key` attribute matches
-    one of those VarKeys, or with strings who match any of the multiple
-    possible string interpretations of each key.
+    """KeyDicts do two things over a regular dict: collate arrays and map keys.
 
     Creating a KeyDict:
     >>>> kd = gpkit.keydict.KeyDict()
+
+    If keymapping is True, a KeyDict keeps an internal list of VarKeys as
+    canonical keys, but allows accessing their values with any object whose
+    `key` attribute matches one of those VarKeys, or with strings who match
+    any of the multiple possible string interpretations of each key:
 
     Now kd[x] can be set, where x is any gpkit Variable or VarKey.
     __getitem__ is such that kd[x] can be accessed using:
@@ -22,7 +22,7 @@ class KeyDict(dict):
      - x.name (a string)
      - "x_modelname" (x's name including modelname)
 
-    In addition, if collapse_arrays is True then VarKeys which have a `shape`
+    If collapse_arrays is True then VarKeys which have a `shape`
     parameter (indicating they are part of an array) are stored as numpy
     arrays, and automatically de-indexed when a matching VarKey with a
     particular `idx` parameter is used as a key.
@@ -33,6 +33,7 @@ class KeyDict(dict):
     See also: gpkit/tests/t_keydict.py.
     """
     collapse_arrays = True
+    keymapping = True
 
     def __init__(self, *args, **kwargs):
         "Passes through to dict.__init__ via the `update()` method"
@@ -44,6 +45,11 @@ class KeyDict(dict):
     def update(self, *args, **kwargs):
         "Iterates through the dictionary created by args and kwargs"
         for k, v in dict(*args, **kwargs).items():
+            if hasattr(v, "copy"):
+                # We don't want just a reference (for e.g. numpy arrays)
+                #   KeyDict values are expected to be immutable (Numbers)
+                #   or to have a copy attribute.
+                v = v.copy()
             self[k] = v
 
     def parse_and_index(self, key):
@@ -112,7 +118,7 @@ class KeyDict(dict):
         key, idx = self.parse_and_index(key)
         if key not in self.keymap:
             self.keymap[key].add(key)
-            if hasattr(key, "keys"):
+            if hasattr(key, "keys") and self.keymapping:
                 for mapkey in key.keys:
                     self.keymap[mapkey].add(key)
             if idx:
@@ -150,11 +156,13 @@ class KeyDict(dict):
                     delete = False
             if delete:
                 dict.__delitem__(self, key)
-                if hasattr(key, "keys"):
-                    for mappedkey in key.keys:
-                        self.keymap[mappedkey].remove(key)
-                        if not self.keymap[mappedkey]:
-                            del self.keymap[mappedkey]
+                mapkeys = set([key])
+                if self.keymapping and hasattr(key, "keys"):
+                    mapkeys.update(key.keys)
+                for mappedkey in mapkeys:
+                    self.keymap[mappedkey].remove(key)
+                    if not self.keymap[mappedkey]:
+                        del self.keymap[mappedkey]
 
 
 class KeySet(KeyDict):
@@ -182,3 +190,8 @@ class KeySet(KeyDict):
     def __setitem__(self, key, value):
         "Assigns the key itself every time."
         KeyDict.__setitem__(self, key, None)
+
+
+class FastKeyDict(KeyDict):
+    "FastKeyDicts are KeyDicts that don't map keys, only collate arrays"
+    keymapping = False
--- gpkit/solution_array.py
+++ gpkit/solution_array.py
@@ -113,15 +113,9 @@ class SolutionArray(DictOfLists):
                     costs = ["%-8.3g" % c for c in mag(subdict[:4])]
                     strs += [" [ %s %s ]" % ("  ".join(costs),
                                              "..." if len(self) > 4 else "")]
-                    cost_units = self.program[0].cost.units
                 else:
                     strs += [" %-.4g" % mag(subdict)]
-                    if hasattr(self.program, "cost"):
-                        cost_units = self.program.cost.units
-                    else:
-                        # we're in a skipsweepfailures that only solved once
-                        cost_units = self.program[0].cost.units
-                strs[-1] += unitstr(cost_units, into=" [%s] ", dimless="")
+                strs[-1] += unitstr(subdict, into=" [%s] ", dimless="")
                 strs += [""]
             elif not subdict:
                 continue
