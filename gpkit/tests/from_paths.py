a8aa05c1ce87224f84d138c9a3457c5917ed0f70
Edward Burnell <eburn@mit.edu>
2016-12-08 23:37:27 -0500

    pylint

--- gpkit/tests/from_paths.py
+++ gpkit/tests/from_paths.py
@@ -1,7 +1,6 @@
 "Runs each file listed in pwd/TESTS as a test"
 
 import unittest
-import os
 import re
 import importlib
 from gpkit import settings
@@ -13,19 +12,22 @@ class TestFiles(unittest.TestCase):
     pass
 
 
-def clean(s):
+def clean(string):
     """Parses string into valid python variable name
 
     https://stackoverflow.com/questions/3303312/
     how-do-i-convert-a-string-to-a-valid-variable-name-in-python
     """
-    s = re.sub('[^0-9a-zA-Z_]', '', s)  # Remove invalid characters
+    string = re.sub('[^0-9a-zA-Z_]', '', string)  # Remove invalid characters
     # Remove leading characters until we find a letter or underscore
-    s = re.sub('^[^a-zA-Z_]+', '', s)
-    return s
+    string = re.sub('^[^a-zA-Z_]+', '', string)
+    return string
 
 
-def add_filetest(TestClass, path):
+def add_filetest(testclass, path):
+    """Add test that imports the given path and runs its test() function
+
+    TODO: make work for subdirectories, using os.chdir"""
     path = path.strip()
     print "adding test for", repr(path)
 
@@ -35,7 +37,7 @@ def add_filetest(TestClass, path):
             self.fail("file '%s' had no `test` function." % path)
         mod.test()
 
-    setattr(TestClass, "test_"+clean(path), test_fn)
+    setattr(testclass, "test_"+clean(path), test_fn)
 
 
 SOLVERS = settings["installed_solvers"]
@@ -45,14 +47,15 @@ def newtest_fn(name, solver, import_dict, path):
     "Doubly nested callbacks to run the test with `getattr(self, name)()`"
     return new_test(name, solver, import_dict, path,
                     testfn=(lambda name, import_dict, path:
-                            lambda self: getattr(self, name)()))
+                            lambda self: getattr(self, name)()))  # pylint:disable=undefined-variable
 
 
 def run(filename="TESTS", xmloutput=False):
+    "Parse and run paths from a given file for each solver"
     with open(filename, "r") as f:
         for path in f:
             add_filetest(TestFiles, path)
-    TESTS = generate_example_tests("", [TestFiles], SOLVERS,
+    tests = generate_example_tests("", [TestFiles], SOLVERS,
                                    newtest_fn=newtest_fn)
-    from gpkit.tests.run_tests import run
-    run(tests=TESTS, unitless=False, xmloutput=xmloutput)
+    from gpkit.tests.run_tests import run as run_
+    run_(tests=tests, unitless=False, xmloutput=xmloutput)
