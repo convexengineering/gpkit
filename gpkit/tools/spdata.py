496caa76b3307aa994408f8d01b3a04a889a4936
Edward Burnell <eburn@mit.edu>
2016-12-09 11:04:43 -0500

    add spdata to tools

---
+++ gpkit/tools/spdata.py
@@ -0,0 +1,54 @@
+import h5py
+import numpy as np
+from ..nomials import *
+from ..geometric_program import genA
+from ..small_scripts import mag
+
+
+class SPData(NomialData):
+    """Generates matrices describing an SP.
+
+    Usage
+    -----
+
+    ``spdata = SPData(m)``
+    ``spdata.save('example_sp.h5')``
+    """
+    def __init__(self, model):
+        if not hasattr(model, "solution"):
+            print "You nede to solve the model first."
+
+        self.signomials = [model.cost]
+        for constraint in model.flat(constraintsets=False):
+            if isinstance(constraint, (SignomialInequality,
+                                       PosynomialInequality)):
+                self.signomials.extend(constraint.unsubbed)
+        NomialData.init_from_nomials(self, self.signomials)
+
+        # k [j]: number of monomials (columns of F) present in each constraint
+        k = [len(p.cs) for p in self.signomials]
+        # p_idxs [i]: posynomial index of each monomial
+        p_idxs = []
+        for i, p_len in enumerate(k):
+            p_idxs += [i]*p_len
+        self.p_idxs = np.array(p_idxs)
+        # A [i, v]: sparse matrix of variable's powers in each monomial
+        self.A, _ = genA(self.exps, self.varlocs)
+
+        self.varsols = np.array([mag(model.solution(var)) for var in self.varlocs])
+        self.varnames = np.array([str(var) for var in self.varlocs])
+
+    def __repr__(self):
+        return str(self.signomials)
+
+    def save(self, filename):
+        "Save spdata to an h5 file."
+        h5f = h5py.File(filename, 'w')
+        try:
+            h5f.create_dataset('cs', data=self.cs)
+            h5f.create_dataset('A', data=self.A.todense())
+            h5f.create_dataset('p_idxs', data=self.p_idxs)
+            h5f.create_dataset('varsols', data=self.varsols)
+            h5f.create_dataset('varnames', data=self.varnames)
+        finally:
+            h5f.close()
