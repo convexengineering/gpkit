0848bec7aa265e9956eeace47cb738f8d7f25fd5
Edward Burnell <eburn@mit.edu>
2016-12-13 16:47:42 -0800

    pylint and no more 'test_repo' branch for externals

--- gpkit/tests/test_repo.py
+++ gpkit/tests/test_repo.py
@@ -1,16 +1,20 @@
+"Implements tests for all external repositories."
 import os
 import sys
 import subprocess
 from time import sleep
 from collections import defaultdict
 
-from gpkit.tests.from_paths import run
 
-
-def test_repos(repos=["d8", "gas_solar_trade"], xmloutput=False):
-    gpkit_models = False
+def test_repos(xmloutput=False):
+    "From gpkit-models gets the list of external repos to test, and tests."
+    git_clone("gpkit-models")
+    pip_install("gpkit-models", local=True)
+    os.chdir("gpkit-models")
+    repos = [line.strip() for line in open("EXTERNALTESTS", "r")]
+    os.chdir("..")
     for repo in repos:
-        git_clone(repo, branch="test_prep")
+        git_clone(repo)
         if os.path.isfile("repo"+os.sep+"setup.py"):
             pip_install(repo, local=True)
         os.chdir(repo)
@@ -20,15 +24,12 @@ def test_repos(repos=["d8", "gas_solar_trade"], xmloutput=False):
         if "gpkit-models branch" in settings:
             branch = settings["gpkit-models branch"]
             os.chdir("..")
-            if not os.path.isdir("gpkit-models"):
-                git_clone("gpkit-models", branch=branch)
-                pip_install("gpkit-models", local=True)
-            else:
-                os.chdir("gpkit-models")
-                call_and_retry(["git", "fetch", "--depth", "1", "origin",
-                                branch])
-                subprocess.call(["git", "checkout", "FETCH_HEAD"])
-                os.chdir("..")
+            os.chdir("gpkit-models")
+            call_and_retry(["git", "fetch", "--depth", "1", "origin",
+                            branch])
+            subprocess.call(["git", "checkout", "FETCH_HEAD"])
+            os.chdir("..")
+            pip_install("gpkit-models", local=True)
             os.chdir(repo)
 
         # install other dependencies
@@ -46,6 +47,7 @@ def test_repos(repos=["d8", "gas_solar_trade"], xmloutput=False):
 
 
 def get_settings():
+    "Gets settings from a TESTCONFIG file"
     settings = defaultdict(str)
     if os.path.isfile("TESTCONFIG"):
         with open("TESTCONFIG", "r") as f:
@@ -56,7 +58,8 @@ def get_settings():
     return settings
 
 
-def git_clone(repo, branch):
+def git_clone(repo, branch="master"):
+    "Tries several times to clone a given repository"
     cmd = ["git", "clone", "--depth", "1"]
     cmd += ["-b", branch]
     cmd += ["https://github.com/hoburg/%s.git" % repo]
@@ -64,11 +67,13 @@ def git_clone(repo, branch):
 
 
 def pip_install(package, local=False):
+    "Tries several times to install a pip package"
     if sys.platform == "win32":
         cmd = ["pip"]
     else:
         cmd = ["python", os.environ["PIP"]]
-    subprocess.call(cmd + ["uninstall", package])
+    if local:  # remove any other local packages of the same name...
+        subprocess.call(cmd + ["uninstall", package])
     cmd += ["install"]
     if local:
         cmd += ["-e"]
@@ -79,6 +84,7 @@ def pip_install(package, local=False):
 
 
 def call_and_retry(cmd, max_iterations=5, delay=5):
+    "Tries max_iterations times (waiting d each time) to run a command"
     iterations = 0
     return_code = None
     print "calling", cmd
