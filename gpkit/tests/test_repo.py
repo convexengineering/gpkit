f6f6a6375ef529d0eaf30972ef976437924f776d
Edward Burnell <eburn@mit.edu>
2016-12-19 18:25:32 -0800

    fix skipsolvers to allow mosek but not mosek_cli

--- gpkit/tests/test_repo.py
+++ gpkit/tests/test_repo.py
@@ -44,9 +44,14 @@ def test_repo(repo=".", xmloutput=False):
             package = package.strip()
             pip_install(package)
 
+    skipsolvers = None
+    if "skipsolvers" in settings:
+        skipsolvers = settings["skipsolvers"].split(",")
+        skipsolvers = map(lambda s: s.strip(), skipsolvers)
+
     testpy = ("from gpkit.tests.from_paths import run;"
               "run(xmloutput=%s, skipsolvers=%s)"
-              % (xmloutput, repr(settings["skipsolvers"])))
+              % (xmloutput, skipsolvers))
     subprocess.call(["python", "-c", testpy])
     if repo != ".":
         os.chdir("..")
