1c3f053b2bde498852f03121b9417d002d8de2bc
Edward Burnell <eburn@mit.edu>
2017-01-14 11:00:24 -0800

    don't require ipywidgets to import from interactive

--- gpkit/interactive/__init__.py
+++ gpkit/interactive/__init__.py
@@ -2,4 +2,3 @@
 from .ractor import ractorjs, ractorpy, showcadtoon
 from .plotting import plot_frontiers
 from .sensitivity_map import SensitivityMap
-from .widgets import modelinteract, modelcontrolpanel
