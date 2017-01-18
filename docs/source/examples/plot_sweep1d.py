178d1cb68fe15a55b117043d04d1e83dae4501a7
Edward Burnell <eburn@mit.edu>
2017-01-17 22:12:09 -0800

    small docs fixes

--- docs/source/examples/plot_sweep1d.py
+++ docs/source/examples/plot_sweep1d.py
@@ -1,7 +1,7 @@
 "Demonstrates manual and auto sweeping and plotting"
 import matplotlib as mpl
 mpl.use('Agg')
-# comment out the imports above and `show()` to show figures in a window
+# comment out the lines above to show figures in a window
 import numpy as np
 from gpkit import Model, Variable, units
 
@@ -13,12 +13,12 @@ m = Model(y, [y >= (x/2)**-0.5 * units.m**2.5 + 1*units.m**2, y >= (x/2)**2])
 sol = m.sweep({x: np.linspace(1, 3, 20)}, verbosity=0)
 f, ax = sol.plot(y)
 ax.set_title("Manually swept (20 points)")
-# f.show()
+f.show()
 f.savefig("plot_sweep1d.png")
 
 # arguments are: model, swept: (min, max, optional logtol), posnomial for y-axis
 sol = m.autosweep({x: (1, 3)}, tol=0.001, verbosity=0)
 f, ax = sol.plot(y)
 ax.set_title("Autoswept (7 points)\nGuaranteed to be in blue region")
-# f.show()
+f.show()
 f.savefig("plot_autosweep1d.png")
