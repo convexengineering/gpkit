209cb15236c0f46b702f483d9536fa102d007740
bqpd <eburn@mit.edu>
2017-03-06 11:08:51 -0500

    simplify and fix sensitivites, add test (#1064)

--- gpkit/nomials/data.py
+++ gpkit/nomials/data.py
@@ -165,10 +165,11 @@ def simplify_exps_and_cs(exps, cs, return_map=False):
         if return_map:
             expmap[exp][i] = cs[i]
 
-    if len(matches) > 1:
-        zeroed_terms = (exp for exp, c in matches.items() if mag(c) == 0)
-        for exp in zeroed_terms:
-            del matches[exp]
+    zeroed_terms = (exp for exp, c in matches.items() if mag(c) == 0)
+    for exp in zeroed_terms:
+        del matches[exp]
+    if not matches:  # it's a zero monomial
+        matches[HashVector()] = 0
 
     exps_ = tuple(matches.keys())
     cs_ = list(matches.values())
