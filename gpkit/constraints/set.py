720b9f9e0685070d111f500f808a3a55fa425925
Edward Burnell <eburn@mit.edu>
2017-05-20 11:30:10 -0400

    cast edits to ConstraintSets

--- gpkit/constraints/set.py
+++ gpkit/constraints/set.py
@@ -96,14 +96,16 @@ class ConstraintSet(list):
         return variables
 
     def __setitem__(self, key, value):
-        if hasattr(value, "substitutions"):
-            self.substitutions.update(value.substitutions)
+        if not isinstance(value, ConstraintSet):
+            value = ConstraintSet(value)
+        self.substitutions.update(value.substitutions)
         list.__setitem__(self, key, value)
         self.reset_varkeys()
 
     def append(self, value):
-        if hasattr(value, "substitutions"):
-            self.substitutions.update(value.substitutions)
+        if not isinstance(value, ConstraintSet):
+            value = ConstraintSet(value)
+        self.substitutions.update(value.substitutions)
         list.append(self, value)
         self.reset_varkeys()
 
