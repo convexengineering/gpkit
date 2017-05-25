7451da604e125476ce855e1debc2643381110e92
Edward Burnell <eburn@mit.edu>
2017-05-24 21:49:56 -0700

    pylint

--- gpkit/solution_array.py
+++ gpkit/solution_array.py
@@ -201,7 +201,7 @@ class SolutionArray(DictOfLists):
             posys = [posys]
         for i, posy in enumerate(posys):
             if posy in [None, "cost"]:
-                posys[i] = self.program[0].cost
+                posys[i] = self.program[0].cost   # pylint: disable=unsubscriptable-object
         import matplotlib.pyplot as plt
         from .interactive.plot_sweep import assign_axes
         from . import GPBLU
