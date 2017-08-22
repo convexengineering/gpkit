b232b698a74da0f91b7ab1c4d4aac0c8706ca2ac
Edward Burnell <eburn@mit.edu>
2017-08-22 12:19:03 -0400

    better debug messages and sensitivity

--- docs/source/examples/debug.py
+++ docs/source/examples/debug.py
@@ -1,9 +1,19 @@
 "Debug examples"
 
-from gpkit import Variable, Model
+from gpkit import Variable, Model, units
+
 x = Variable("x", "ft")
 x_min = Variable("x_min", 2, "ft")
 x_max = Variable("x_max", 1, "ft")
 y = Variable("y", "volts")
+
 m = Model(x/y, [x <= x_max, x >= x_min])
-m.debug(verbosity=0)
+m.debug()
+
+print "# Now let's try a model unsolvable with relaxed constants\n"
+
+Model(x, [x <= units("inch"), x >= units("yard")]).debug()
+
+print "# And one that's only unbounded\n"
+
+Model(x/y, [x >= x_min]).debug()
