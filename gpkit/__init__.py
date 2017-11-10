9855ce83c399b829081480daa475b063fb589320
bqpd <eburn@mit.edu>
2017-11-10 01:38:25 -0500

    add requirements file for RTD (#1230)

--- gpkit/__init__.py
+++ gpkit/__init__.py
@@ -1,10 +1,7 @@
 "GP and SP modeling package"
 __version__ = "0.6.0"
 
-try:
-    from ._pint import units, ureg, DimensionalityError
-except ImportError:  # allow readthedocs to import without pint
-    pass
+from ._pint import units, ureg, DimensionalityError
 from .globals import settings
 from .globals import SignomialsEnabled, SIGNOMIALS_ENABLED
 from .globals import Vectorize, VECTORIZATION
