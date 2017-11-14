c050ce95018fb7beb5abfa633dda7707a8188fae
Edward Burnell <eburn@mit.edu>
2017-11-13 21:20:08 -0500

    clean up performance modeling example

--- docs/source/examples/performance_modeling.py
+++ docs/source/examples/performance_modeling.py
@@ -1,21 +1,24 @@
 """Modular aircraft concept"""
 import numpy as np
-from gpkit import Model, Variable, Vectorize
+from gpkit import Model, Variable, Vectorize, parse_variables
 
 
 class Aircraft(Model):
     """The vehicle model
+
+    Variables
+    ---------
+    W  [lbf]  weight
 
     Upper Unbounded
     ---------------
     W
     """
     def setup(self):
+        exec parse_variables(Aircraft.__doc__)
         self.fuse = Fuselage()
         self.wing = Wing()
         self.components = [self.fuse, self.wing]
-
-        W = self.W = Variable("W", "lbf", "weight")
 
         return self.components, [
             W >= sum(c.topvar("W") for c in self.components)
@@ -29,9 +32,10 @@ class Aircraft(Model):
 class AircraftP(Model):
     """Aircraft flight physics: weight <= lift, fuel burn
 
-    Submodels
+    Variables
     ---------
-    aircraft, state, wing_aero
+    Wfuel  [lbf]  fuel weight
+    Wburn  [lbf]  segment fuel burn
 
     Upper Unbounded
     ---------------  # c will be bounded by Aircraft
@@ -42,34 +46,37 @@ class AircraftP(Model):
     Wfuel, W
     """
     def setup(self, aircraft, state):
+        exec parse_variables(AircraftP.__doc__)
         self.aircraft = aircraft
         self.wing_aero = aircraft.wing.dynamic(state)
         self.perf_models = [self.wing_aero]
-        Wfuel = self.Wfuel = Variable("W_{fuel}", "lbf", "fuel weight")
-        Wburn = self.Wburn = Variable("W_{burn}", "lbf", "segment fuel burn")
 
-        D = self.wing_aero["D"]
-        Re = self.wing_aero["Re"]
-        self.c = aircraft.wing.c
         W = self.W = aircraft.topvar("W")
-        rho = state["\\rho"]
-        V = state["V"]
-        CL = self.CL = self.wing_aero["C_L"]
-        S = aircraft.wing["S"]
+        c = self.c = aircraft.wing.c
+        S = aircraft.wing.S
 
-        return self.perf_models, [
-            W + Wfuel <= 0.5*rho*CL*S*V**2,
-            Wburn >= 0.1*D
-            ]
+        V = state.V
+        rho = state.rho
 
-from gpkit.constraints.set import ConstraintSet
+        D = self.wing_aero.D
+        Re = self.wing_aero.Re
+        CL = self.wing_aero.CL
+
+        return [W + Wfuel <= 0.5*rho*CL*S*V**2,
+                Wburn >= 0.1*D], self.perf_models
 
 class FlightState(Model):
-    "Context for evaluating flight physics"
+    """Context for evaluating flight physics
+
+    Variables
+    ---------
+    V    40        [knots]    true airspeed
+    mu    1.628e-5 [N*s/m^2]  dynamic viscosity
+    rho   0.74     [kg/m^3]   air density
+
+    """
     def setup(self):
-        Variable("V", 40, "knots", "true airspeed")
-        Variable("\\mu", 1.628e-5, "N*s/m^2", "dynamic viscosity")
-        Variable("\\rho", 0.74, "kg/m^3", "air density")
+        exec parse_variables(FlightState.__doc__)
 
 
 class FlightSegment(Model):
@@ -111,8 +118,8 @@ class Mission(Model):
         with Vectorize(4):  # four flight segments
             self.fs = FlightSegment(aircraft)
 
-        Wburn = self.fs.aircraftp["W_{burn}"]
-        Wfuel = self.fs.aircraftp["W_{fuel}"]
+        Wburn = self.fs.aircraftp.Wburn
+        Wfuel = self.fs.aircraftp.Wfuel
         self.takeoff_fuel = Wfuel[0]
         self.W = aircraft.W
         self.c = aircraft.wing.c
@@ -124,27 +131,37 @@ class Mission(Model):
 class Wing(Model):
     """Aircraft wing model
 
+    Variables
+    ---------
+    W        [lbf]       weight
+    S    190 [ft^2]      surface area
+    rho    1 [lbf/ft^2]  areal density
+    A     27 [-]         aspect ratio
+    c        [ft]        mean chord
+
     Upper Unbounded
     ---------------
     W
     """
+    def setup(self):
+        exec parse_variables(Wing.__doc__)
+        return [W >= S*rho,
+                c == (S/A)**0.5]
+
     def dynamic(self, state):
         "Returns this component's performance model for a given state."
         return WingAero(self, state)
 
-    def setup(self):
-        W = self.W = Variable("W", "lbf", "weight")
-        S = Variable("S", 190, "ft^2", "surface area")
-        rho = Variable("\\rho", 1, "lbf/ft^2", "areal density")
-        A = Variable("A", 27, "-", "aspect ratio")
-        c = self.c = Variable("c", "ft", "mean chord")
-
-        return [W >= S*rho,
-                c == (S/A)**0.5]
-
-
 class WingAero(Model):
     """Wing aerodynamics
+
+    Variables
+    ---------
+    CD      [-]    drag coefficient
+    CL      [-]    lift coefficient
+    e   0.9 [-]    Oswald efficiency
+    Re      [-]    Reynold's number
+    D       [lbf]  drag force
 
 
     Upper Unbounded
@@ -156,31 +173,28 @@ class WingAero(Model):
     CL
     """
     def setup(self, wing, state):
-        CD = Variable("C_D", "-", "drag coefficient")
-        CL = self.CL = Variable("C_L", "-", "lift coefficient")
-        e = Variable("e", 0.9, "-", "Oswald efficiency")
-        Re = self.Re = Variable("Re", "-", "Reynold's number")
-        D = self.D = Variable("D", "lbf", "drag force")
+        exec parse_variables(WingAero.__doc__)
         self.c = wing.c
 
         return [
-            CD >= (0.074/Re**0.2 + CL**2/np.pi/wing["A"]/e),
-            Re == state["\\rho"]*state["V"]*wing["c"]/state["\\mu"],
-            D >= 0.5*state["\\rho"]*state["V"]**2*CD*wing["S"],
+            CD >= (0.074/Re**0.2 + CL**2/np.pi/wing.A/e),
+            Re == state.rho*state.V*wing.c/state.mu,
+            D >= 0.5*state.rho*state.V**2*CD*wing.S,
             ]
 
 
 class Fuselage(Model):
-    "The thing that carries the fuel, engine, and payload"
+    """The thing that carries the fuel, engine, and payload
+
+    A full model is left as an exercise for the reader.
+
+    Variables
+    ---------
+    W  100 [lbf]  weight
+
+    """
     def setup(self):
-        # fuselage needs an external dynamic drag model,
-        # left as an exercise for the reader
-        # V = Variable("V", 16, "gal", "volume")
-        # d = Variable("d", 12, "in", "diameter")
-        # S = Variable("S", "ft^2", "wetted area")
-        # cd = Variable("c_d", .0047, "-", "drag coefficient")
-        # CDA = Variable("CDA", "ft^2", "drag area")
-        Variable("W", 100, "lbf", "weight")
+        exec parse_variables(Fuselage.__doc__)
 
 AC = Aircraft()
 MISSION = Mission(AC)
