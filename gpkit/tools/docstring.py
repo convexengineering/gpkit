b7bfae2275779c7b921bb6d17160115f032d56f2
Ned Burnell <eburn@mit.edu>
2018-03-20 14:12:24 -0700

    allow vectorvariables to be parsed without needing variables (#1274)

--- gpkit/tools/docstring.py
+++ gpkit/tools/docstring.py
@@ -128,43 +128,43 @@ def parse_variables(string):
                                " = Variable('{0}', '{1}', '{2}')\n")
                         outstr += out.format(nameval[0], units, label)
             string = string[len(flag):]
-        string = ostring
-        flag = "Variables of length"
-        count = string.count(flag)
-        if count:
-            outstr += "\nfrom gpkit import VectorVariable\n"
-            for _ in range(count):
-                idx = string.index(flag)
-                string = string[idx:]
-                idx2 = string.index("\n")
-                length = string[len(flag):idx2].strip()
-                string = string[idx2:]
-                if idx == -1:
-                    idx = 0
-                    skiplines = 0
-                else:
-                    skiplines = 2
-                for line in string.split("\n")[skiplines:]:
-                    try:
-                        unitstart, unitend = line.index("["), line.index("]")
-                    except ValueError:
-                        break
-                    units = line[unitstart+1:unitend]
-                    labelstart = unitend + 1
-                    if labelstart < len(line):
-                        while line[labelstart] == " ":
-                            labelstart += 1
-                        label = line[labelstart:].replace("'", "\\'")
-                        nameval = line[:unitstart].split()
-                        if len(nameval) == 2:
-                            out = ("{0} = self.{0} = VectorVariable({4},"
-                                   " '{0}', {1}, '{2}', '{3}')\n")
-                            outstr += out.format(nameval[0], nameval[1],
-                                                 units, label, length)
-                        elif len(nameval) == 1:
-                            out = ("{0} = self.{0} = VectorVariable({3},"
-                                   " '{0}', '{1}', '{2}')\n")
-                            outstr += out.format(nameval[0], units, label,
-                                                 length)
-                string = string[len(flag):]
+    string = ostring
+    flag = "Variables of length"
+    count = string.count(flag)
+    if count:
+        outstr += "\nfrom gpkit import VectorVariable\n"
+        for _ in range(count):
+            idx = string.index(flag)
+            string = string[idx:]
+            idx2 = string.index("\n")
+            length = string[len(flag):idx2].strip()
+            string = string[idx2:]
+            if idx == -1:
+                idx = 0
+                skiplines = 0
+            else:
+                skiplines = 2
+            for line in string.split("\n")[skiplines:]:
+                try:
+                    unitstart, unitend = line.index("["), line.index("]")
+                except ValueError:
+                    break
+                units = line[unitstart+1:unitend]
+                labelstart = unitend + 1
+                if labelstart < len(line):
+                    while line[labelstart] == " ":
+                        labelstart += 1
+                    label = line[labelstart:].replace("'", "\\'")
+                    nameval = line[:unitstart].split()
+                    if len(nameval) == 2:
+                        out = ("{0} = self.{0} = VectorVariable({4},"
+                               " '{0}', {1}, '{2}', '{3}')\n")
+                        outstr += out.format(nameval[0], nameval[1],
+                                             units, label, length)
+                    elif len(nameval) == 1:
+                        out = ("{0} = self.{0} = VectorVariable({3},"
+                               " '{0}', '{1}', '{2}')\n")
+                        outstr += out.format(nameval[0], units, label,
+                                             length)
+            string = string[len(flag):]
     return outstr
