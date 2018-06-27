abe04e2d9a1add3e1d2872601d00469488940e59
1ozturkbe <1ozturkbe@gmail.com>
2018-06-27 13:29:53 -0400

    Reverting solution_array.

--- gpkit/solution_array.py
+++ gpkit/solution_array.py
@@ -490,7 +490,8 @@ def results_table(data, title, printunits=True, fixedcols=True,
             if not sortbyvals:
                 decorated.append((model, b, (varfmt % s), i, k, v))
             else:  # for consistent sorting, add small offset to negative vals
-                val = np.mean(np.abs(v)) - (1e-7 if np.mean(v) < 0 else 0)
+                val = np.mean(np.abs(v)) - (1e-9 if np.mean(v) < 0 else 0)
+                val -= hash(k.name)*1e-30
                 decorated.append((model, -val, b, (varfmt % s), i, k, v))
     if included_models:
         included_models = set(included_models)
