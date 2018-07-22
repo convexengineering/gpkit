19c12d6dbabaaba5dd542237c08d546b31b37301
Edward Burnell <eburn@mit.edu>
2018-07-22 13:43:32 -0400

    show relaxval in printout

--- gpkit/constraints/model.py
+++ gpkit/constraints/model.py
@@ -262,14 +262,18 @@ class Model(CostedConstraintSet):
                     print("\nSolves with these constraints relaxed:")
                     for relaxval, i in relaxed:
                         constraint = feas[0][0][i][0]
+                        # substitutions of the final relax value
+                        conleft = constraint.left.sub(
+                            {constrsrelaxed.relaxvars[i]: relaxval})
+                        conright = constraint.right.sub(
+                            {constrsrelaxed.relaxvars[i]: relaxval})
                         origconstraint = constrsrelaxed.origconstrs[i]
                         relax_percent = "%i%%" % (0.5+(relaxval-1)*100)
                         print(" %3i: %5s relaxed, from %s %s %s \n"
                               "                     to %s %s %s "
                               % (i, relax_percent, origconstraint.left,
                                  origconstraint.oper, origconstraint.right,
-                                 constraint.left, constraint.oper,
-                                 constraint.right))
+                                 conleft, constraint.oper, conright))
                 print("\n>> Success!")
             except (ValueError, RuntimeWarning):
                 print(">> Failure")
