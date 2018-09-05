fix solution array diff units (#1352)

gpkit/solution_array.py
@@ -74,8 +74,8 @@ def reldiff(val1, val2):
             val1_dims = len(val1.shape)
             if (hasattr(val2, "shape")
                     and val2.shape[:val1_dims] == val1.shape):
-                val1 = np.tile(val1.magnitude, val2.shape[val1_dims:]+(1,)).T
-                val1 = val1 * val1.units
+                val1_ = np.tile(val1.magnitude, val2.shape[val1_dims:]+(1,)).T
+                val1 = val1_ * val1.units
         # numpy division will warn but return infs
         return (val2/val1 - 1).to("dimensionless").magnitude
     elif val2.magnitude == 0:  # both are scalar zeroes