a302f6ed06380d5755cf8b335c4346ff86a46f44
rileyjmurray <inventor1488@gmail.com>
2020-01-20 22:32:17 -0800

    improved documentation for mskoptimize in mosek_conif.py

--- gpkit/_mosek/mosek_conif.py
+++ gpkit/_mosek/mosek_conif.py
@@ -21,7 +21,7 @@ def mskoptimize(c, A, k, p_idxs, *args, **kwargs):
     k : ints array of shape p+1
         k[0] is the number of monomials (rows of A) present in the objective
         k[1:] is the number of monomials (rows of A) present in each constraint
-    p_idxs : list of bool arrays, each of shape m
+    p_idxs : ints array of shape n.
         sel = p_idxs == i selects rows of A and entries of c of the i-th posynomial
         fi(x) = c[sel] @ exp(A[sel,:] @ x). The 0-th posynomial gives the objective
         function, and the remaining posynomials should be constrained to be <= 1.
@@ -30,133 +30,144 @@ def mskoptimize(c, A, k, p_idxs, *args, **kwargs):
     -------
     dict
         Contains the following keys
-            "success": bool
-            "objective_sol" float
-                Optimal value of the objective
-            "primal_sol": floats array of size m
-                Optimal value of free variables. Note: not in logspace.
-            "dual_sol": floats array of size p
-                Optimal value of the dual variables, in logspace.
+            "status": string
+                "optimal", "infeasible", "unbounded", or "unknown".
+            "primal" np.ndarray or None
+                The values of the ``m`` primal variables.
+            "la": np.ndarray or None
+                The dual variables to the ``p`` posynomial constraints, when
+                those constraints are represented in log-sum-exp ("LSE") form.
     """
     import mosek
     #
-    #   Prepare some initial problem data
+    #   Initial transformations of problem data.
     #
-    #       Say that the optimization variable is y = [x;t;z], where
-    #       x is of length m, t is of length 3*n, and z is of length p+1.
+    #       separate monomial constraints (call them "lin"), from those which require
+    #       an LSE representation (call those "lse").
     #
-    #       Triples
-    #           t[3*ell]
-    #           t[3*ell + 1] should be == 1.
-    #           t[3*ell + 2] should be == A[ell, :] @ x + np.log(c[ell]) - z[posynom idx corresponding to ell].
-    #       should belong to the exponential cone.
+    #       NOTE: the epigraph of the objective function always gets an "lse"
+    #       representation, even if the objective is a monomial.
     #
-    #       For each i from {0,...,p}, the "t" should also satisfy
-    #           sum(t[3*ell] for ell correponsing to posynomial i) <= 1.
-    #
-    #       The vector "z" should satisfy
-    #           z[1:] <= 0
-    #
-    #
-    c = np.array(c)
-    # separate exponential from linear constraints.
-    # the objective is always kept in exponential form.
-    exp_posys = [0] + [i+1 for i, val in enumerate(k[1:]) if val > 1]
-    lin_posys = [i for i in range(len(k)) if i not in exp_posys]
+    log_c = np.log(np.array(c))
+    lse_posys = [0] + [i+1 for i, val in enumerate(k[1:]) if val > 1]
+    lin_posys = [i for i in range(len(k)) if i not in lse_posys]
     if len(lin_posys) > 0:
         A = A.tocsr()
-        lin_idxs = []
-        for i in lin_posys:
-            temp = np.nonzero(p_idxs == i)
-            temp = temp[0]
-            lin_idxs.append(temp)
-        lin_idxs = np.concatenate(lin_idxs)
-        nonlin_idxs = np.ones(A.shape[0], dtype=bool)
-        nonlin_idxs[lin_idxs] = False
-        A_exp = A[nonlin_idxs, :].tocoo()
-        c_exp = c[nonlin_idxs]
+        lin_idxs = np.concatenate([np.nonzero(p_idxs == i)[0] for i in lin_posys])
+        lse_idxs = np.ones(A.shape[0], dtype=bool)
+        lse_idxs[lin_idxs] = False
+        A_lse = A[lse_idxs, :].tocoo()
+        log_c_lse = log_c[lse_idxs]
         A_lin = A[lin_idxs, :].tocoo()
-        c_lin = c[lin_idxs]
+        log_c_lin = log_c[lin_idxs]
     else:
-        c_lin = np.array([])
-        A_exp = A
-        c_exp = c
-
-    m = A.shape[1]
-    k_exp = [k[i] for i in exp_posys]
-    n_exp = sum(k_exp)
-    p_exp = len(k_exp)
-    n_msk = m + 3*n_exp + p_exp
-    exp_p_idx = []
-    for i, ki in enumerate(k_exp):
-        exp_p_idx.extend([i] * ki)
-    exp_p_idx = np.array(exp_p_idx)
+        log_c_lin = np.array([])  # A_lin won't be referenced later, so no need to define it.
+        A_lse = A
+        log_c_lse = log_c
+    k_lse = [k[i] for i in lse_posys]
+    n_lse = sum(k_lse)
+    p_lse = len(k_lse)
+    lse_p_idx = []
+    for i, ki in enumerate(k_lse):
+        lse_p_idx.extend([i] * ki)
+    lse_p_idx = np.array(lse_p_idx)
     #
-    #   Create MOSEK task. add variables and conic constraints.
+    #   Create MOSEK task. Add variables, and conic constraints.
+    #
+    #       Say that MOSEK's optimization variable is a block vector, [x;t;z], where ...
+    #           x is the user-defined primal variable (length m),
+    #           t is an auxiliary variable for exponential cones (length 3 * n_lse), and
+    #           z is an epigraph variable for LSE terms (length p_lse).
+    #
+    #       The variable z[0] is special, because it's the epigraph of the objective function
+    #       in LSE form. The sign of this variable is not constrained.
+    #
+    #       The variables z[1:] are epigraph terms for "log", in constraints that naturally
+    #       write as LSE(Ai @ x + log_ci) <= 0. These variables need to be <= 0.
+    #
+    #       The main constraints on (x, t, z) are described in next comment block.
     #
     env = mosek.Env()
     task = env.Task(0, 0)
-    task.appendvars(n_msk)
-    bound_types = [mosek.boundkey.fr] * (m + 3*n_exp + 1) + [mosek.boundkey.up] * (p_exp - 1)
-    task.putvarboundlist(np.arange(n_msk, dtype=int),
-                         bound_types, np.zeros(n_msk), np.zeros(n_msk))
-    for i in range(n_exp):
+    m = A.shape[1]
+    msk_nvars = m + 3 * n_lse + p_lse
+    task.appendvars(msk_nvars)
+    bound_types = [mosek.boundkey.fr] * (m + 3*n_lse + 1) + [mosek.boundkey.up] * (p_lse - 1)
+    task.putvarboundlist(np.arange(msk_nvars, dtype=int),
+                         bound_types, np.zeros(msk_nvars), np.zeros(msk_nvars))
+    for i in range(n_lse):
         idx = m + 3*i
         task.appendcone(mosek.conetype.pexp, 0.0, np.arange(idx, idx + 3))
     #
     #   Affine constraints related to the exponential cone
     #
-    task.appendcons(2*n_exp + p_exp)
-    # 1st n_exp: Linear equations: t[3*ell + 1] == 1
-    rows = [i for i in range(n_exp)]
-    cols = (m + 3*np.arange(n_exp) + 1).tolist()
-    vals = [1.0] * n_exp
+    #       For each i in {0, ..., n_lse - 1}, we need
+    #           t[3*i + 1] == 1, and
+    #           t[3*i + 2] == A_lse[i, :] @ x + log_c_lse[i] - z[lse_p_idx[i]].
+    #       This contributes 2 * n_lse constraints.
+    #
+    #       For each j from {0, ..., p_lse - 1}, the "t" should also satisfy
+    #           sum(t[3*i] for i where i == lse_p_idx[j]) <= 1.
+    #       This contributes another p_lse constraints.
+    #
+    #       The above constraints imply that for ``sel = lse_p_idx == i``, we have
+    #           LSE(A_lse[sel, :] @ x + log_c_lse[sel]) <= z[i].
+    #
+    #       We specify the necessary constraints to MOSEK in three phases. Over the
+    #       course of these three phases, we make a total of five calls to "putaijlist"
+    #       and a single call to "putconboundlist".
+    #
+    task.appendcons(2*n_lse + p_lse)
+    # 1st n_lse: Linear equations: t[3*i + 1] == 1
+    rows = [i for i in range(n_lse)]
+    cols = (m + 3*np.arange(n_lse) + 1).tolist()
+    vals = [1.0] * n_lse
     task.putaijlist(rows, cols, vals)
-    # 2nd n_exp: Linear equations:
-    #       A[ell,:] @ x  - t[3*ell + 2] - z[posynom idx corresponding to ell] == -np.log(c[ell])
-    cur_con_idx = n_exp
-    rows = [cur_con_idx + r for r in A_exp.row]
-    task.putaijlist(rows, A_exp.col, A_exp.data)
-    rows = [cur_con_idx + i for i in range(n_exp)]
-    cols = (m + 3*np.arange(n_exp) + 2).tolist()
-    vals = [-1.0] * n_exp
-    task.putaijlist(rows, cols, vals)
-    rows = [cur_con_idx + i for i in range(n_exp)]
-    cols = [m + 3*n_exp + exp_p_idx[i] for i in range(n_exp)]
-    vals = [-1.0] * n_exp
-    task.putaijlist(rows, cols, vals)
-    # last p_exp: Linear inequalities:
-    #       1 @ t[3 * sels] <= 1, for sels = np.nonzero(exp_p_idxs[i] == i)
-    cur_con_idx = 2*n_exp
+    cur_con_idx = n_lse
+    # 2nd n_lse: Linear equations between (x,t,z).
+    rows = [cur_con_idx + r for r in A_lse.row]
+    task.putaijlist(rows, A_lse.col, A_lse.data)  # coefficients on "x"
+    rows = [cur_con_idx + i for i in range(n_lse)]
+    cols = (m + 3*np.arange(n_lse) + 2).tolist()
+    vals = [-1.0] * n_lse
+    task.putaijlist(rows, cols, vals)  # coefficients on "t"
+    rows = [cur_con_idx + i for i in range(n_lse)]
+    cols = [m + 3*n_lse + lse_p_idx[i] for i in range(n_lse)]
+    vals = [-1.0] * n_lse
+    task.putaijlist(rows, cols, vals)  # coefficients on "z".
+    cur_con_idx = 2 * n_lse
+    # last p_lse: Linear inequalities on certain sums of "t".
     rows, cols, vals = [], [], []
-    for i in range(p_exp):
-        sels = np.nonzero(exp_p_idx == i)[0]
+    for i in range(p_lse):
+        sels = np.nonzero(lse_p_idx == i)[0]
         rows.extend([cur_con_idx] * sels.size)
         cols.extend(m + 3 * sels)
         vals.extend([1] * sels.size)
         cur_con_idx += 1
     task.putaijlist(rows, cols, vals)
+    cur_con_idx = 2 * n_lse + p_lse
     # Build the right-hand-sides of the [in]equality constraints
-    type_constraint = [mosek.boundkey.fx] * (2*n_exp) + [mosek.boundkey.up] * p_exp
-    h = np.concatenate([np.ones(n_exp), -np.log(c_exp), np.ones(p_exp)])
+    type_constraint = [mosek.boundkey.fx] * (2*n_lse) + [mosek.boundkey.up] * p_lse
+    h = np.concatenate([np.ones(n_lse), -log_c_lse, np.ones(p_lse)])
     task.putconboundlist(np.arange(h.size, dtype=int), type_constraint, h, h)
     #
     #   Affine constraints, not needing the exponential cone
     #
-    cur_con_idx = 2*n_exp + p_exp
-    if c_lin.size > 0:
-        task.appendcons(c_lin.size)
+    #       Require A_lin @ x <= -log_c_lin.
+    #
+    if log_c_lin.size > 0:
+        task.appendcons(log_c_lin.size)
         rows = [cur_con_idx + r for r in A_lin.row]
         task.putaijlist(rows, A_lin.col, A_lin.data)
-        type_constraint = [mosek.boundkey.up] * c_lin.size
-        con_indices = np.arange(cur_con_idx, cur_con_idx + c_lin.size, dtype=int)
-        h = -np.log(c_lin)
+        type_constraint = [mosek.boundkey.up] * log_c_lin.size
+        con_indices = np.arange(cur_con_idx, cur_con_idx + log_c_lin.size, dtype=int)
+        h = -log_c_lin
         task.putconboundlist(con_indices, type_constraint, h, h)
-        cur_con_idx += c_lin.size
+        cur_con_idx += log_c_lin.size
     #
     #   Set the objective function
     #
-    task.putclist([int(m + 3*n_exp)], [1])
+    task.putclist([int(m + 3*n_lse)], [1])
     task.putobjsense(mosek.objsense.minimize)
     #
     #   Set solver parameters, and call .solve().
@@ -192,19 +203,19 @@ def mskoptimize(c, A, k, p_idxs, *args, **kwargs):
         x = np.array(x)
         # recover dual variables for log-sum-exp epigraph constraints
         # (skip epigraph of the objective function).
-        z_duals = [0.] * (p_exp - 1)
-        task.getsuxslice(mosek.soltype.itr, m + 3*n_exp + 1, n_msk, z_duals)
+        z_duals = [0.] * (p_lse - 1)
+        task.getsuxslice(mosek.soltype.itr, m + 3*n_lse + 1, msk_nvars, z_duals)
         z_duals = np.array(z_duals)
         z_duals[z_duals < 0] = 0
         # recover dual variables for the remaining user-provided constraints
-        if c_lin.size > 0:
-            aff_duals = [0.] * c_lin.size
-            task.getsucslice(mosek.soltype.itr, 2*n_exp + p_exp, cur_con_idx, aff_duals)
+        if log_c_lin.size > 0:
+            aff_duals = [0.] * log_c_lin.size
+            task.getsucslice(mosek.soltype.itr, 2*n_lse + p_lse, cur_con_idx, aff_duals)
             aff_duals = np.array(aff_duals)
             aff_duals[aff_duals < 0] = 0
             # merge z_duals with aff_duals
             merged_duals = np.zeros(len(k))
-            merged_duals[exp_posys[1:]] = z_duals
+            merged_duals[lse_posys[1:]] = z_duals
             merged_duals[lin_posys] = aff_duals
             merged_duals = merged_duals[1:]
         else:
@@ -212,11 +223,11 @@ def mskoptimize(c, A, k, p_idxs, *args, **kwargs):
         # wrap things up in a dictionary
         solution = {'status': 'optimal', 'primal': x, 'la': merged_duals}
     elif msk_solsta == mosek.solsta.prim_infeas_cer:
-        solution = {'status': 'infeasible', 'primal': None}
+        solution = {'status': 'infeasible', 'primal': None, 'la': None}
     elif msk_solsta == mosek.solsta.dual_infeas_cer:
-        solution = {'status': 'unbounded', 'primal': None}
+        solution = {'status': 'unbounded', 'primal': None, 'la': None}
     else:
-        solution = {'status': 'unknown', 'primal': None}
+        solution = {'status': 'unknown', 'primal': None, 'la': None}
     task.__exit__(None, None, None)
     env.__exit__(None, None, None)
     return solution
