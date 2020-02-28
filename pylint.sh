fef77959cde9367d1ec57324d85bf555d4c2a51d
Edward Burnell <eburn@mit.edu>
2020-02-27 19:20:18 -0800

    check pylint name

--- pylint.sh
+++ pylint.sh
@@ -5,7 +5,13 @@ WORKSPACE=$(git rev-parse --show-toplevel)
 
 # Calling pylint directly will not work correcly in a virtualenv if pylint is not installed in the venv
 # Using python with the pylint script will always work properly in a virtualenv
-PYLINT=`which pylint3`
+if [[ `which pylint3` ]]; then
+  PYLINT=`which pylint3`
+else
+  PYLINT=`which pylint`
+fi
+which $PYLINT
+$PYLINT --version
 
 # Add gpkit to the python path so that pylint can import gpkit when analyzing the examples directory
 export PYTHONPATH=$PYTHONPATH:$WORKSPACE/gpkit/
