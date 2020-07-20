fc12f0866dd91f9dc47e849fe898f5d9cf025332
mtwichan <mtwichan@gmail.com>
2020-07-20 00:43:31 -0700

    Remove potientially unecessary assert statements?

--- docs/source/examples/evaluated_free_variables.py
+++ docs/source/examples/evaluated_free_variables.py
@@ -1,7 +1,8 @@
+from gpkit import Variable, Model
+
 # code from t_constraints.test_evalfn in tests/t_sub.py
 x = Variable("x")
 x2 = Variable("x^2", evalfn=lambda v: v[x]**2)
 m = Model(x, [x >= 2])
 m.unique_varkeys = set([x2.key])
 sol = m.solve(verbosity=0)
-self.assertAlmostEqual(sol(x2), sol(x)**2)
