e88870ba1d8d552f0f3f99468590cea2ea5e2463
mtwichan <mtwichan@gmail.com>
2020-08-01 06:23:54 -0700

    Breakdown gpkit global import

--- docs/source/examples/using_var_sens_1.py
+++ docs/source/examples/using_var_sens_1.py
@@ -1,8 +1,8 @@
 "Example variable sensitivity usage"
 import math
-import gpkit
-x = gpkit.Variable("x")
-x_min = gpkit.Variable("x_{min}", 2)
-sol = gpkit.Model(x, [x_min <= x]).solve()
+from gpkit import Variable, Model
+x = Variable("x")
+x_min = Variable("x_{min}", 2)
+sol = Model(x, [x_min <= x]).solve()
 sens_x_min = sol["sensitivities"]["variables"][x_min]
 assert math.isclose(sens_x_min, 1.00, rel_tol=0.01)
--- docs/source/examples/vectorization.py
+++ docs/source/examples/vectorization.py
@@ -1,11 +1,11 @@
 "Example Vectorize usage, from gpkit/tests/t_vars.py"
-import gpkit
+from gpkit import Variable, Vectorize, VectorVariable
 
-with gpkit.Vectorize(3):
-    with gpkit.Vectorize(5):
-        y = gpkit.Variable("y")
-        x = gpkit.VectorVariable(2, "x")
-    z = gpkit.VectorVariable(7, "z")
+with Vectorize(3):
+    with Vectorize(5):
+        y = Variable("y")
+        x = VectorVariable(2, "x")
+    z = VectorVariable(7, "z")
 
 assert(y.shape == (5, 3))
 assert(x.shape == (2, 5, 3))
