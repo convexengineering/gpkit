e58e56dd1c7c3ced41e76e130b7eeb5202d7ae14
Edward Burnell <eburn@mit.edu>
2022-01-08 15:55:12 -0700

    fix

--- docs/source/examples/breakdowns.py
+++ docs/source/examples/breakdowns.py
@@ -1,53 +1,51 @@
 "An example to show off Breakdowns"
 import os
-import sys
 import pickle
+import pint
 from gpkit.breakdowns import Breakdowns
 
-# the code to create solar.p is in ./breakdowns/solartest.py
-filepath = os.path.dirname(os.path.realpath(__file__)) + os.sep + "solar.p"
-sol = pickle.load(open(filepath, "rb"))
-bds = Breakdowns(sol)
+if pint.__version__.split(".") > ["0", "9"]:
+    # the code to create solar.p is in ./breakdowns/solartest.py
+    filepath = os.path.dirname(os.path.realpath(__file__)) + os.sep + "solar.p"
+    sol = pickle.load(open(filepath, "rb"))
+    bds = Breakdowns(sol)
 
-print("Cost breakdown (you may be familiar with this from solution tables)")
-print("==============")
-try:
+    print("Cost breakdown (as seen in solution tables)")
+    print("==============")
     bds.plot("cost")
-except AttributeError:  # if the pint version differs from the pickled solution
-    sys.exit(0)
 
-print("Variable breakdowns (note the two methods of access)")
-print("===================")
-varkey, = sol["variables"].keymap[("Mission.FlightSegment.AircraftPerf"
-                                   ".AircraftDrag.Poper")]
-bds.plot(varkey)
-bds.plot("AircraftPerf.AircraftDrag.MotorPerf.Q")
+    print("Variable breakdowns (note the two methods of access)")
+    print("===================")
+    varkey, = sol["variables"].keymap[("Mission.FlightSegment.AircraftPerf"
+                                       ".AircraftDrag.Poper")]
+    bds.plot(varkey)
+    bds.plot("AircraftPerf.AircraftDrag.MotorPerf.Q")
 
-print("Combining the two above by increasing maxwidth")
-print("----------------------------------------------")
-bds.plot("AircraftPerf.AircraftDrag.Poper", maxwidth=105)
+    print("Combining the two above by increasing maxwidth")
+    print("----------------------------------------------")
+    bds.plot("AircraftPerf.AircraftDrag.Poper", maxwidth=105)
 
-print("Model sensitivity breakdowns (note the two methods of access)")
-print("============================")
-bds.plot("model sensitivities")
-bds.plot("Aircraft")
+    print("Model sensitivity breakdowns (note the two methods of access)")
+    print("============================")
+    bds.plot("model sensitivities")
+    bds.plot("Aircraft")
 
-print("Exhaustive variable breakdown traces (and configuration arguments)")
-print("====================================")
-# often useful as a reference point when reading traces
-bds.plot("AircraftPerf.AircraftDrag.Poper", height=12)
-# includes factors, can be useful for reading traces as well
-bds.plot("AircraftPerf.AircraftDrag.Poper", showlegend=True)
-print("\nPermissivity = 2 (the default)")
-print("----------------")
-bds.trace("AircraftPerf.AircraftDrag.Poper")
-print("\nPermissivity = 1 (stops at Pelec = v·i)")
-print("----------------")
-bds.trace("AircraftPerf.AircraftDrag.Poper", permissivity=1)
+    print("Exhaustive variable breakdown traces (and configuration arguments)")
+    print("====================================")
+    # often useful as a reference point when reading traces
+    bds.plot("AircraftPerf.AircraftDrag.Poper", height=12)
+    # includes factors, can be useful for reading traces as well
+    bds.plot("AircraftPerf.AircraftDrag.Poper", showlegend=True)
+    print("\nPermissivity = 2 (the default)")
+    print("----------------")
+    bds.trace("AircraftPerf.AircraftDrag.Poper")
+    print("\nPermissivity = 1 (stops at Pelec = v·i)")
+    print("----------------")
+    bds.trace("AircraftPerf.AircraftDrag.Poper", permissivity=1)
 
-# you can also produce Plotly treemaps/icicle plots of your breakdowns
-fig = bds.treemap("model sensitivities", returnfig=True)
-fig = bds.icicle("cost", returnfig=True)
-# uncommenting any of the below makes and shows the plot directly
-# bds.icicle("model sensitivities")
-# bds.treemap("cost")
+    # you can also produce Plotly treemaps/icicle plots of your breakdowns
+    fig = bds.treemap("model sensitivities", returnfig=True)
+    fig = bds.icicle("cost", returnfig=True)
+    # uncommenting any of the below makes and shows the plot directly
+    # bds.icicle("model sensitivities")
+    # bds.treemap("cost")
