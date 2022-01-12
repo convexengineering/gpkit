577fe80c161bc407184b06f03d11c3915c5e5e0f
Edward Burnell <eburn@mit.edu>
2022-01-12 13:33:17 -0700

    Update year

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 LICENSE = """The MIT License (MIT)
 
-Copyright (c) 2020 Edward Burnell
+Copyright (c) 2022 Edward Burnell
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
