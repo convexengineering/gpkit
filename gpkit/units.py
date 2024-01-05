ac2a959c0d8677bf8c7c2c797b54b29b16e2f678
Warren Hoburg <whoburg@alum.mit.edu>
2024-01-05 05:22:48 -0600

    explicitly raise from

--- gpkit/units.py
+++ gpkit/units.py
@@ -23,7 +23,7 @@ class GPkitUnits:
 
     def __call__(self, unity):
         "Returns a unit Monomial, caching the result for future retrievals"
-        from . import Monomial
+        from . import Monomial  # pylint: disable=import-outside-toplevel
         if unity not in self.monomial_cache:
             self.monomial_cache[unity] = Monomial(qty(unity))
         return self.monomial_cache[unity]
@@ -44,8 +44,8 @@ class GPkitUnits:
                 conversion = numerator.units or 1/denominator.units
             try:
                 self.division_cache[key] = float(conversion)
-            except DimensionalityError:
-                raise DimensionalityError(numerator, denominator)
+            except DimensionalityError as exc:
+                raise DimensionalityError(numerator, denominator) from exc
         return self.division_cache[key]
 
     def of_product(self, thing1, thing2):
