d2105581235c52aef55470846df03ba2ab59dd81
Warren Hoburg <whoburg@alum.mit.edu>
2024-01-07 19:05:26 -0600

    pylint fixes

--- docs/source/examples/autosweep.py
+++ docs/source/examples/autosweep.py
@@ -12,18 +12,19 @@ l = Variable("l", "m")
 m1 = Model(A**2, [A >= l**2 + units.m**2])
 tol1 = 1e-3
 bst1 = autosweep_1d(m1, tol1, l, [1, 10], verbosity=0)
-print("Solved after %2i passes, cost logtol +/-%.3g" % (bst1.nsols, bst1.tol))
+print(f"Solved after {bst1.nsols:2} passes, cost logtol +/-{bst1.tol:.3g}")
 # autosweep solution accessing
 l_vals = np.linspace(1, 10, 10)
 sol1 = bst1.sample_at(l_vals)
-print("values of l: %s" % l_vals)
-print("values of A: [%s] %s" %
-      (" ".join("% .1f" % n for n in sol1("A").magnitude), sol1("A").units))
+print(f"values of l: {l_vals}")
+print("values of A: [" +
+      " ".join(f"{n: .1f}" for n in sol1("A").magnitude) + "] " +
+      str(sol1("A").units))
 cost_estimate = sol1["cost"]
 cost_lb, cost_ub = sol1.cost_lb(), sol1.cost_ub()
-print("cost lower bound:\n%s\n" % cost_lb)
-print("cost estimate:\n%s\n" % cost_estimate)
-print("cost upper bound:\n%s\n" % cost_ub)
+print(f"cost lower bound:\n{cost_lb}\n")
+print(f"cost estimate:\n{cost_estimate}\n")
+print(f"cost upper bound:\n{cost_ub}\n")
 # you can evaluate arbitrary posynomials
 np.testing.assert_allclose(mag(2*sol1(A)), mag(sol1(2*A)))
 assert (sol1["cost"] == sol1(A**2)).all()
@@ -32,7 +33,8 @@ np.testing.assert_allclose((np.log(mag(cost_lb)) + np.log(mag(cost_ub)))/2,
                            np.log(mag(cost_estimate)))
 # save autosweep to a file and retrieve it
 bst1.save("autosweep.pkl")
-bst1_loaded = pickle.load(open("autosweep.pkl", "rb"))
+with open ("autosweep.pkl", "rb") as f:
+    bst1_loaded = pickle.load(f)
 
 # this problem is two intersecting lines in logspace
 m2 = Model(A**2, [A >= (l/3)**2, A >= (l/3)**0.5 * units.m**1.5])
@@ -41,6 +43,6 @@ tol2 = {"mosek_cli": 1e-6, "mosek_conif": 1e-6,
 # test Model method
 sol2 = m2.autosweep({l: [1, 10]}, tol2, verbosity=0)
 bst2 = sol2.bst
-print("Solved after %2i passes, cost logtol +/-%.3g" % (bst2.nsols, bst2.tol))
+print(f"Solved after {bst2.nsols:2} passes, cost logtol +/-{bst2.tol:.3g}")
 print("Table of solutions used in the autosweep:")
 print(bst2.solarray.table())
